34bf28e68e807d12f2d8dd45692fea2f9772857a
hexinping <hexinping456@126.com>
2018-12-25 10:25:23 +0800

    １

---
+++ UnityLearn/Assets/Scripts/Script.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour {
+
+    private GameObject gCloneObj;
+	// Use this for initialization
+	void Start () {
+		
+        //创建游戏对象
+        GameObject gCreateObj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        
+        //添加材质
+        gCreateObj.GetComponent<Renderer>().material.color = Color.red;
+
+        //添加名称
+        gCreateObj.name = "CubeName";
+
+        //克隆对象
+        gCloneObj = (GameObject)GameObject.Instantiate(gCreateObj); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//销毁对象
+        if (Input.GetKey(KeyCode.D))
+        {
+            Destroy(gCloneObj, 2F); //2F表示2秒后销毁，没有参数立即销毁
+        }
+	}
+}
