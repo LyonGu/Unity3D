0cbdf9737b70282d5047f6be8e8729b031e93645
hexinping <hexinping456@126.com>
2018-12-25 19:07:15 +0800

    UGUI控件

---
+++ UnityLearn/Assets/Scripts/AddingButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class AddingButton : MonoBehaviour {
+
+    public GameObject goParent;
+	// Use this for initialization
+	void Start () {
+		//脚本创建一个button对象
+
+        GameObject goNewObject = new GameObject("Button");
+
+        //设置button的父对象
+        goNewObject.GetComponent<Transform>().SetParent(goParent.GetComponent<Transform>(),false);
+
+        //设置button的显示外观
+        Image image = goNewObject.AddComponent<Image>();
+        Button btn = goNewObject.AddComponent<Button>();
+
+        image.overrideSprite = Resources.Load<Sprite>("Resources/Textures/login_select.png");
+
+        //btn监听事件
+        btn.onClick.AddListener(ProcessSomething);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ProcessSomething()
+    {
+        print("button 动态添加");
+    }
+}
---
+++ UnityLearn/Assets/Scripts/ButtonEvent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonEvent : MonoBehaviour {
+
+    public Text Text_DisplayInfo;   //信息显示控件
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void DisplayInfo()
+    {
+        Debug.Log("Button 被击中了！！！！！");
+    }
+
+    public void DisplayInfoByText()
+    {
+        Text_DisplayInfo.text = "Button 被击中了";
+    }
+}
