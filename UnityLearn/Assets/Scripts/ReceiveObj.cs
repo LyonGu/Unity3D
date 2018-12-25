ef1acad6f700844433c9b6586cd6b9f2a40a9c11
hexinping <hexinping456@126.com>
2018-12-25 22:33:15 +0800

    数据传值

---
+++ UnityLearn/Assets/Scripts/ReceiveObj.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReceiveObj : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void DisplayInfo_A()
+    {
+        print(GetType() + "/DisplayInfo_A 无参方法");
+    }
+
+    public void DisplayInfo_B()
+    {
+        print(GetType() + "/DisplayInfo_B 无参方法");
+    }
+
+    public void DisplayInfo_2(string info)
+    {
+        print(GetType() + "/DisplayInfo_2 带一个字符串参数，info = " + info);
+    }
+
+    public void DisplayInfo_3(int info)
+    {
+        print(GetType() + "/DisplayInfo_3 带一个整形参数，info = " + info);
+    }
+
+
+    public void DisplayInfo_4(System.Object[] objArray)
+    {
+        print(GetType() + "/DisplayInfo_4 objArray[0] = " + objArray[0]);
+        print(GetType() + "/DisplayInfo_4 objArray[1] = " + objArray[1]);
+        print(GetType() + "/DisplayInfo_4 objArray[2] = " + objArray[2]);
+    }
+
+}
---
+++ UnityLearn/Assets/Scripts/SendObj.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendObj : MonoBehaviour {
+
+    //接收对象
+    public GameObject goReceiveObj;
+
+
+	// Use this for initialization
+	void Start () {
+
+        //怎么感觉就在调用其他对象的方法就是了
+
+
+        //发消息
+        goReceiveObj.SendMessage("DisplayInfo_A");
+
+        goReceiveObj.SendMessage("DisplayInfo_B", SendMessageOptions.RequireReceiver); //如果消息发送不成功，则报错
+
+       // goReceiveObj.SendMessage("DisplayInfo_B", SendMessageOptions.DontRequireReceiver); //如果消息发送不成功，不报错
+
+        goReceiveObj.SendMessage("DisplayInfo_2","大家好"); // 带参数
+        goReceiveObj.SendMessage("DisplayInfo_3", 999);
+
+        System.Object[] objArray = new System.Object[3];
+        objArray[0] = "dajasdfsdf";
+        objArray[1] = 88;
+        objArray[2] = 66.6F;
+
+        goReceiveObj.SendMessage("DisplayInfo_4", objArray);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
