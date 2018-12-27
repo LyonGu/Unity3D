ffd503dac8eb4048513cabac6820520b68194f8d
hexinping <hexinping456@126.com>
2018-12-27 17:03:13 +0800

    删除无用文件，添加第三方资源包

---
+++ UnityLearn/Assets/Scripts/BaseNavigation.cs
@@ -0,0 +1,41 @@
+﻿/***
+ *
+ *  Title: 
+ *         第21章 导航寻路
+ *         
+ *  Description:
+ *        功能：
+ *            基本导航控制脚本
+ *           
+ *
+ *  Date: 2017
+ * 
+ *  Version: 1.0
+ *
+ *  Modify Recorder:
+ *     
+ */
+using UnityEngine;
+using System.Collections;
+
+public class BaseNavigation : MonoBehaviour{
+    //寻路目标
+    public Transform TraFindDestination;      
+    //寻路组件
+    private UnityEngine.AI.NavMeshAgent _Agent;                          
+
+
+    void Start(){
+        _Agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }//Start_end
+
+
+    void Update(){
+        //设置寻路
+        if (_Agent && TraFindDestination)
+        {
+            _Agent.SetDestination(TraFindDestination.transform.position);
+        }
+    }//Update_end
+}
+
---
+++ UnityLearn/Assets/Scripts/NavmeshObstacleControl.cs
@@ -0,0 +1,46 @@
+﻿/***
+ *
+ *  Title: 
+ *         第21章 导航寻路
+ *         
+ *  Description:
+ *        功能：
+ *            寻路障碍物控制脚本
+ *           
+ *
+ *  Date: 2017
+ * 
+ *  Version: 1.0
+ *
+ *  Modify Recorder:
+ *     
+ */
+using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;                       //导入AI命名空间
+
+public class NavmeshObstacleControl : MonoBehaviour
+{
+    private NavMeshObstacle _navMeshObs;    //路径障碍组件
+
+    void Start(){
+        _navMeshObs = this.GetComponent<NavMeshObstacle>();
+    }
+
+    void Update(){
+        if (Input.GetButtonDown("Fire1")){  //允许通过               
+            if (_navMeshObs){
+                _navMeshObs.enabled = false;               
+                this.GetComponent<Renderer>().material.color = Color.green;
+            }
+        }
+
+        if (Input.GetButtonUp("Fire1")){    //禁止通过               
+            if (_navMeshObs){
+                _navMeshObs.enabled = true;
+                this.GetComponent<Renderer>().material.color = Color.red;
+            }
+        }
+    }
+}
+
