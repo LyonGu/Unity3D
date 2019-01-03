341faf83b79352baadd27566a03a2ff8b950d83a
hexinping <hexinping456@126.com>
2019-01-03 16:42:07 +0800

    1

--- UnityLearn/Assets/Scripts/HelloWorld.cs
+++ UnityLearn/Assets/Scripts/HelloWorld.cs
@@ -7,6 +7,11 @@ public class HelloWorld : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Debug.Log("Hello world");
+
+        GameObject camera = GameObject.FindGameObjectWithTag("MainCamera");
+
+        GameObject testCube = camera.transform.Find("testCube").gameObject;//取某个gameobject的子gameobject
+        testCube.GetComponent<MeshRenderer>().material.color = Color.blue;
 	}
 	
 	// Update is called once per frame
