00f404983c00a503af352714b68022e429751d7e
hexinping <hexinping456@126.com>
2019-01-10 11:55:39 +0800

    测试调用成功

---
+++ xlua_Unity/Assets/CSLua.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+public class CSLua : MonoBehaviour {
+
+    private LuaEnv luaEnv;
+
+
+
+	// Use this for initialization
+	void Start () {
+        luaEnv = new LuaEnv();
+        luaEnv.DoString("print('lua形式打印输出')");
+        luaEnv.DoString("CS.UnityEngine.Debug.Log('cs 形式打印输出')");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void onDestory()
+    {
+        luaEnv.Dispose();
+    }
+}
