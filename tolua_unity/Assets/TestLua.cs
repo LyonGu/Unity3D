53107719beb0fa7e54e9b08acdfa6e2627ea26ba
hexinping <hexinping456@126.com>
2019-01-11 19:41:18 +0800

    unity中导入tolua

---
+++ tolua_unity/Assets/TestLua.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LuaInterface;
+
+public class TestLua : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        LuaState lua = new LuaState();
+        lua.Start();
+        string hello =
+            @"                
+                print('hello tolua')                                  
+            ";
+
+        lua.DoString(hello, "HelloWorld.cs");
+        lua.CheckTop();
+        lua.Dispose();
+        lua = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
