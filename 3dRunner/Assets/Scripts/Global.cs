d2d593fd7c9b1a985add1235ebecd1d27a417641
hexinping <hexinping456@126.com>
2019-02-11 18:56:52 +0800

    添加全局数据脚本

---
+++ 3dRunner/Assets/Scripts/Global.cs
@@ -0,0 +1,41 @@
+﻿
+/***
+ *
+ *  Title: "3DRunner" 项目
+ *         项目全局变量定义
+ *
+ *  Description:
+ *        功能：
+ *        1： 游戏状态
+ *        2： 游戏速度、分数、距离等。
+ *
+ *  Date: 2017
+ * 
+ *  Version: 1.0
+ *
+ *  Modify Recorder:
+ *     
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Runner3D
+{
+   
+    public class Global : MonoBehaviour {
+
+	    // Use this for initialization
+	    void Start () {
+		
+	    }
+	
+	    // Update is called once per frame
+	    void Update () {
+		
+	    }
+    }
+}
+
