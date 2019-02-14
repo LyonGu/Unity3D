1671c22f892419784e31fcef8011c2f72640813b
hexinping <hexinping456@126.com>
2019-02-15 07:54:40 +0800

    添加 场景道具管理器脚本

---
+++ 3dRunner/Assets/Scripts/ScenesCreateMgr.cs
@@ -0,0 +1,37 @@
+﻿
+/***
+ *
+ *  Title: "3DRunner" 项目
+ *         描述：场景创建管理器
+ *
+ *  Description:
+ *        功能：
+ *        1：大型场景道具的创建与删除
+ *        2：小型道具的创建与删除
+ *       
+ *
+ *  Date: 2019
+ * 
+ *  Version: 1.0
+ *
+ *  Modify Recorder:
+ *     
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ScenesCreateMgr : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
