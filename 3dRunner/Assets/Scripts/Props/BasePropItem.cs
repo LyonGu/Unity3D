30bd51cd606306f9306bb665fca40aeb81921f3b
hexinping <hexinping456@126.com>
2019-02-16 12:14:23 +0800

    添加资源

--- 3dRunner/Assets/Scripts/Props/BasePropItem.cs
+++ 3dRunner/Assets/Scripts/Props/BasePropItem.cs
@@ -15,7 +15,7 @@
  *  Modify Recorder:
  *     
  */
-
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -23,13 +23,123 @@ using UnityEngine.UI;
 
 public class BasePropItem : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+
+    //道具出发委托
+    public Action m_PropTriggerHandle;
+
+    /// 道具（接触英雄）触发检测
+    protected void OnTriggerEnter(Collider col)
+    { 
+        if(col.tag.Equals(Global.HeroTagName))
+        {
+            if (m_PropTriggerHandle!=null)
+            {
+                m_PropTriggerHandle.Invoke();
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// 销毁自身
+    /// </summary>
+    /// <param name="destoryTarget">销毁目标对象</param>
+    /// <param name="destoryDelay">销毁延迟时间</param>
+    protected void EnableDestory(GameObject destroyTarget, int destoryDelay = 0)
+    { 
+        if(destroyTarget!= null)
+        {
+            Destroy(destroyTarget, destoryDelay);
+        }
+    }
+
+
+    /// <summary>
+    /// 启动粒子特效
+    /// 备注：1秒后自动销毁
+    /// </summary>
+    /// <param name="particlePrefab">粒子预设</param>
+    /// <param name="displayPos">显示方位</param>
+    /// <param name="parentNode">父节点</param>
+
+    protected void EnableParticleEffect(GameObject particlePrefab, Vector3 displayPos, Transform parentNode = null)
+    {
+        ClonePrefabs(particlePrefab, displayPos, parentNode, 1);
+    }
+
+
+    /// <summary>
+    /// 克隆游戏对象预设
+    /// </summary>
+    /// <param name="goPrefab">游戏对象预设体</param>
+    /// <param name="displayPos">显示方位</param>
+    /// <param name="parentNode">父对象节点(默认无父对象)</param>
+    /// <param name="destoryTime">销毁时间(默认不销毁)</param>
+    public void ClonePrefabs(GameObject goPrefab, Vector3 displayPos, Transform parentNode = null, int destoryTime = 0)
+    {
+        //参数检查
+        if (goPrefab == null || displayPos == Vector3.zero)
+        {
+            Debug.LogError(GetType() + "/EnableParticleEffect()/goPrefab==null Or displayPos==null ! 请检查错误");
+        }
+
+
+        GameObject goClone = Instantiate(goPrefab, displayPos,Quaternion.identity);
+        if (parentNode!=null)
+        {
+            //父节点定义
+            goClone.transform.parent = parentNode;
+        }
+
+        //定义粒子预设的相对位移
+        goClone.transform.localPosition = new Vector3(goClone.transform.localPosition.x, goClone.transform.localPosition.y , goClone.transform.localPosition.z);
+        //定时销毁(1秒定时)
+        if (destoryTime > 0)
+        {
+            EnableDestory(goClone, destoryTime);
+        }
+    }
+
+
+    /// <summary>
+    /// 得到指定范围的随机整数
+    /// </summary>
+    /// <param name="minNum">最小数值</param>
+    /// <param name="MaxNum">最大数值</param>
+    /// <returns></returns>
+    public int GetRandomNum(int minNum, int maxNum)
+    {
+        int randomNumResult = 0;
+
+        if (minNum == maxNum)
+        {
+            randomNumResult = minNum;
+        }
+        randomNumResult = UnityEngine.Random.Range(minNum, maxNum + 1);
+        return randomNumResult;
+    }
+
+    /// <summary>
+    /// 概率生成算法
+    /// </summary>
+    /// <param name="number">概率数值（1/number）</param>
+    /// <returns>
+    /// true: 得到概率
+    /// false; 没有得到
+    /// </returns>
+    public bool GetProbability(int number)
+    {
+        bool boolResult = false;
+        int randomNum = 999;
+
+        randomNum = GetRandomNum(0, number);
+        if (randomNum == 0)
+        {
+            boolResult = true;
+        }
+
+        return boolResult;
+    }
+
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
--- 3dRunner/Assets/Scripts/ScenesCreateMgr.cs
+++ 3dRunner/Assets/Scripts/ScenesCreateMgr.cs
@@ -23,15 +23,120 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class ScenesCreateMgr : MonoBehaviour {
+public class ScenesCreateMgr : BasePropItem {
+
+    //场景建筑预设
+    public GameObject BuildingPrefab = null;
+
+    //场景预设的父节点
+    public Transform ParentNodeByBuildPrefab = null; 
+
+    //场景生成点预设
+    public GameObject BuildingCreatePrefab = null;
+
+    //金币预设
+    public GameObject CoinPrefabs = null;
+
+    //障碍物预设集合
+    public GameObject[] ObstaclesPrefabsArray = null;
+
+    //魔法道具预设集合
+    public GameObject[] MagicProPrefabsArray = null;
+
+    //生成道具的父节点
+    public Transform ParentNodeByProp = null;
+    //道具定位参考坐标点
+    public Transform _PropRefPostion = null;
+    //是否已经生成金币
+    private bool _IsProduceCoin = false;   
 
 	// Use this for initialization
 	void Start () {
-		
+		//参数检查
+        //if (BuildingPrefab == null || BuildingCreatePrefab == null || _PropRefPostion == null)
+        //{
+        //    Debug.LogError(GetType() + "/Start()/脚本参数有错误，请改正。");
+        //}
+
+        //初始道具集合
+        CreateProps();
+        //初始建筑集合
+        CreateBuildings();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    /// <summary>
+    /// 生成道具预设
+    /// 核心算法：
+    /// 	 A： 一个“建筑物群” ，长度32米。一次生成2个，所以就一次生成64米的“场景道路”。
+    ///      B： 一般以6米作为一个生成道具的基础单位。 所以每一次生成道具，都循环10次。64/6=10（次）
+    ///      C:  每个跑道，对于金币、道具(障碍物/魔法道具)都各占1/3的生成概率。
+    /// </summary>
+    private void CreateProps()
+    {
+        for (int i = 0; i < 10;i++ )
+        {
+             //(本循环)道具Z轴长度数值
+            float floZLengthNumber = 0F;
+            _IsProduceCoin = false;
+            floZLengthNumber = Global.ZposByCurrentBuilds + 30F + i * Global.ZLengthEveryFloor;
+
+            //生成金币道具
+            ProduceCoins(base.GetRandomNum(2, 4), new Vector3(Global.LeftTrackX, _PropRefPostion.position.y, floZLengthNumber));
+        }
+    }
+
+    /// 生成场景建筑预设
+    private void CreateBuildings()
+    {
+        for (int i = 1; i <= 2; i++)
+        {
+            //通过预设动态生成gameobject
+            GameObject goBuildingClone = Instantiate(BuildingPrefab,new Vector3(0,0,Global.ZposByCurrentBuilds + Global.ZLengthByBuildPrefab*i), Quaternion.identity);
+            //确定父子节点
+            if (ParentNodeByBuildPrefab != null)
+            {
+                goBuildingClone.transform.parent = ParentNodeByBuildPrefab;
+            }
+
+            if (i == 1)
+            {
+                //感觉没什么用
+                GameObject goBuildTriggerPosClone = Instantiate(BuildingCreatePrefab,
+                        new Vector3(0, 0, Global.ZposByCurrentBuilds + Global.ZLengthByBuildPrefab * i), Quaternion.identity);
+                goBuildTriggerPosClone.transform.parent = ParentNodeByBuildPrefab;
+            }
+        
+        }
+        //更新建筑物当前长度数值
+        Global.ZposByCurrentBuilds += Global.ZLengthByBuildPrefab * 2;
+    }
+
+
+    /// <summary>
+    /// 生成金币
+    /// </summary>
+    /// <param name="produceNum">生成数量</param>
+    /// <param name="pos">金币方位</param>
+    /// <param name="parentNode">父节点</param>
+    private void ProduceCoins(int produceNum, Vector3 pos)
+    {
+        //参数检查
+        if (CoinPrefabs == null || pos == Vector3.zero || produceNum <= 0 || ParentNodeByProp == null)
+        {
+            Debug.LogError(GetType() + "/ProduceCoins()/参数有误， 请检查。");
+        }
+
+        for (int i = 0; i < produceNum; i++)
+        {
+            base.ClonePrefabs(CoinPrefabs, new Vector3(pos.x, pos.y, pos.z + i * Global.IntervalOfCoins), ParentNodeByProp);
+        }
+    }
+
 }
+
