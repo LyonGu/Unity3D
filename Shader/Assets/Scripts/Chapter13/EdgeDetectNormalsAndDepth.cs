9c8e7e5af6a6b91bd170658c1722350ba27cff9a
hexinping <hexinping456@126.com>
2019-03-23 12:23:30 +0800

    边缘检测采用深度纹理实现

---
+++ Shader/Assets/Scripts/Chapter13/EdgeDetectNormalsAndDepth.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EdgeDetectNormalsAndDepth : PostEffectsBase {
+
+	public Shader edgeDetectShader;
+	private Material edgeDetectMaterial = null;
+	public Material material {  
+		get {
+			edgeDetectMaterial = CheckShaderAndCreateMaterial(edgeDetectShader, edgeDetectMaterial);
+			return edgeDetectMaterial;
+		}  
+	}
+
+	[Range(0.0f, 1.0f)]
+	public float edgesOnly = 0.0f;
+
+	public Color edgeColor = Color.black;
+
+	public Color backgroundColor = Color.white;
+
+    //控制对深度+法线纹理的采样，sampleDistance越大，描边越宽
+	public float sampleDistance = 1.0f;
+
+    //领域判断是否存在边的阈值
+	public float sensitivityDepth = 1.0f;
+
+	public float sensitivityNormals = 1.0f;
+	
+	void OnEnable() {
+        //获取深度+法线纹理
+		GetComponent<Camera>().depthTextureMode |= DepthTextureMode.DepthNormals;
+	}
+
+    //默认情况下OnRenderImage会在所有透明和不透明物体渲染完才掉用，ImageEffectOpaque标签让渲染完不透明物体直接调用，不对透明物体产生影响
+    //这个例子只希望对不透明物体产生描边
+	[ImageEffectOpaque]
+	void OnRenderImage (RenderTexture src, RenderTexture dest) {
+		if (material != null) {
+			material.SetFloat("_EdgeOnly", edgesOnly);
+			material.SetColor("_EdgeColor", edgeColor);
+			material.SetColor("_BackgroundColor", backgroundColor);
+			material.SetFloat("_SampleDistance", sampleDistance);
+			material.SetVector("_Sensitivity", new Vector4(sensitivityNormals, sensitivityDepth, 0.0f, 0.0f));
+
+			Graphics.Blit(src, dest, material);
+		} else {
+			Graphics.Blit(src, dest);
+		}
+	}
+}
