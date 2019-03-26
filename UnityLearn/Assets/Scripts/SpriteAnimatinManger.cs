a25df0f5dfb7f564e11314946a38820e2df158a5
hexinping <hexinping456@126.com>
2019-03-26 16:21:57 +0800

    添加精灵帧管理类实现： 播放，循环播放，暂停，加速减速，切换动作，切换方向

---
+++ UnityLearn/Assets/Scripts/SpriteAnimatinManger.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteAnimatinManger
+{
+
+
+    private static SpriteAnimatinManger _instance = null;
+
+    private Dictionary<string, Object[]> _cacheMap = null;
+	// Use this for initialization
+
+    public SpriteAnimatinManger()
+    {
+        _cacheMap = new Dictionary<string, Object[]>();
+    }
+    static public SpriteAnimatinManger getInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new SpriteAnimatinManger();
+        }
+        return _instance;
+    }
+
+    public Object[] LoadPlistResource(string file)
+    {
+        if (!_cacheMap.ContainsKey(file))
+        {
+            //加载整一张图集，此方法会返回一个Object[]，里面包含了图集的纹理 Texture2D和图集下的全部Sprite
+            Object[] _atlas = Resources.LoadAll("Plist/" + file);
+            _cacheMap[file] = _atlas;
+        }
+        return _cacheMap[file];
+    }
+
+    public void removePlistResource(string file)
+    {
+        Object[] _atlas = _cacheMap[file];
+        if (_atlas != null)
+        {
+            for (int i = 1; i < _atlas.Length; i++)
+            {
+                unloadAsset(_atlas[i]);
+            }
+            _cacheMap[file] = null;
+        }
+    }
+
+    public void unloadAsset(Object obj)
+    {
+        Resources.UnloadAsset(obj);
+    }
+       
+}
