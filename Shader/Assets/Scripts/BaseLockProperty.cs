715011137722ae4d76a2258392696052966decbb
hexinping <hexinping456@126.com>
2019-03-27 17:23:13 +0800

    使用材质属性锁定 确保不同objec使用统一个材质 shader调整参数导致其他object也发生变化

---
+++ Shader/Assets/Scripts/BaseLockProperty.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseLockProperty : MonoBehaviour
+{
+
+    protected MaterialPropertyBlock _propertyBlock;
+
+    void Awake()
+    {
+        _propertyBlock = new MaterialPropertyBlock();
+        //intPropertys();
+    }
+
+
+    void OnGUI()
+    {
+        GetComponent<Renderer>().GetPropertyBlock(_propertyBlock);
+        onChangePropertys();
+        GetComponent<Renderer>().SetPropertyBlock(_propertyBlock);
+    }
+
+    virtual public void onChangePropertys()
+    { 
+    
+    }
+
+    virtual public void intPropertys()
+    { 
+    
+    }
+
+}
---
+++ Shader/Assets/Scripts/LockProperty.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockProperty : BaseLockProperty
+{
+
+    public Color _EmissiveColor;
+    public Color _AmbientColor;
+    public float _MySliderValue;
+
+    public override void intPropertys()
+    {
+        _EmissiveColor = new Color32(165,118,118,255);  
+        _AmbientColor = new Color32(154, 77, 77, 255);
+        _MySliderValue = 2.5F;
+    }
+    public override void onChangePropertys()
+    {
+        _propertyBlock.SetColor("_AmbientColor", _AmbientColor);
+        _propertyBlock.SetColor("_AmbientColor", _AmbientColor);
+        _propertyBlock.SetFloat("_MySliderValue", _MySliderValue);
+    }
+}
