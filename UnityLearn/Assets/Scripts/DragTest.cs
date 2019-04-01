99a8bd1ac016f0860e221113416073ccc11353e4
hexinping <hexinping456@126.com>
2019-04-01 11:53:18 +0800

    Drag事件

---
+++ UnityLearn/Assets/Scripts/DragTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragTest : MonoBehaviour,IDragHandler ,IPointerDownHandler {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void DragUpdate()
+    {
+        Debug.Log("DragUpdate=================");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag=================");
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown=================");
+    }
+}
