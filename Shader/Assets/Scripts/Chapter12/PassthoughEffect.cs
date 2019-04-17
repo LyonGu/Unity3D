8ab2602390f3825f233f994c53ffd34dc5005662
hexinping <hexinping456@126.com>
2019-04-17 15:51:51 +0800

    添加时空扭曲后期处理

---
+++ Shader/Assets/Scripts/Chapter12/PassthoughEffect.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class PassthoughEffect : PostEffectsBase{
+
+    public Shader passthoughShader;
+    private Material passthoughMaterial;
+
+
+    public Material material
+    {
+        get {
+            passthoughMaterial = CheckShaderAndCreateMaterial(passthoughShader, passthoughMaterial);
+            return passthoughMaterial;
+        }
+    }
+
+    //扭曲强度
+    [Range(0, 1)]
+    public float distortFactor = 0.0f;
+    //扭曲中心（0-1）屏幕空间，默认为中心点
+    public Vector2 distortCenter = new Vector2(0.5f, 0.5f);
+
+    //噪声图
+    public Texture NoiseTexture = null;
+    //屏幕扰动强度
+    [Range(0, 2.0f)]
+    public float distortStrength = 1.0f;
+
+    //屏幕收缩总时间
+    public float passThoughTime = 1.0f;
+    //当前时间
+    private float currentTime = 0.0f;
+    //曲线控制权重
+    public float curveFactor = 0.2f;
+    //屏幕收缩效果曲线控制
+    public AnimationCurve curve;
+
+
+    //扰动曲线系数
+    public float distortCurveFactor = 1.0f;
+    //屏幕扰动效果曲线控制
+    public AnimationCurve distortCurve;
+
+    private float _maxPass = 0.3f;
+
+    public void Start()
+    {
+        currentTime = 0.0f;
+        StartCoroutine("UpdatePassthoughEffect");
+    }
+
+    void CreateOrOPenFile(string pathName, string info)
+    {          //路径、文件名、写入内容
+        StreamWriter sw;
+        FileInfo fi = new FileInfo(pathName);
+        sw = fi.CreateText();        //直接重新写入，如果要在原文件后面追加内容，应用fi.AppendText()
+        sw.WriteLine(info);
+        sw.Close();
+        sw.Dispose();
+    }
+
+    private IEnumerator UpdatePassthoughEffect()
+    {
+
+        string result1 = "";
+        string result2 = "";
+        while (currentTime < passThoughTime)
+        {
+            currentTime += Time.deltaTime;
+            float t = currentTime / passThoughTime;
+
+            float test1 = (float)(Mathf.Round(curve.Evaluate(t) * 100))/100;
+            float test2 = (float)(Mathf.Round(distortCurve.Evaluate(t) * 100)) / 100;
+            result1 = result1 + test1 + ",";
+            result2 = result2 + test2 + ",";
+           
+            //根据时间占比在曲线（0，1）区间采样，再乘以权重作为收缩系数
+            distortFactor = curve.Evaluate(t) * curveFactor;
+            distortStrength = distortCurve.Evaluate(t) * distortCurveFactor;
+            yield return null;
+            //结束时强行设置为0
+            distortFactor = 0.0f;
+            distortStrength = 0.0f;
+
+        }
+
+        CreateOrOPenFile("config/1.txt", result1);
+        CreateOrOPenFile("config/2.txt", result2);
+
+    }
+
+
+
+    //OnRenderImage绘制绘制完所有透明和不透明的物体后每一帧调用
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (material != null)
+        {
+            currentTime += Time.deltaTime;
+            float t = currentTime / passThoughTime;
+            //distortFactor = Mathf.Lerp(0, 1, t * curveFactor);
+
+            //if (currentTime >= passThoughTime*0.5)
+            //{
+            //    distortStrength = Mathf.Lerp(0, 1, t * distortCurveFactor);
+            //}
+            
+
+            //if (currentTime >= passThoughTime)
+            //{
+            //    distortFactor = 0;
+            //    distortStrength = 0;
+            //}
+
+
+          
+
+            //设置shader属性值
+            material.SetTexture("_NoiseTex", NoiseTexture);
+            material.SetFloat("_DistortFactor", distortFactor);
+            material.SetVector("_DistortCenter", distortCenter);
+            material.SetFloat("_DistortStrength", distortStrength);
+
+            //把屏幕纹理src传进shader里_MainTex属性
+            Graphics.Blit(src, dest, material);
+        }
+        else
+        {
+            Graphics.Blit(src, dest);
+        }
+    }
+
+}
---
+++ Shader/Assets/Shaders/Chapter12/Passthough.shader
@@ -0,0 +1,74 @@
+﻿Shader "Shaders/Chapter12/Passthough"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_NoiseTex("Noise", 2D) = "black"{}
+	}
+	SubShader
+	{
+		ZTest Always Cull Off ZWrite Off
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma fragmentoption ARB_precision_hint_fastest
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			
+			#include "UnityCG.cginc"
+
+			struct a2v
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			uniform float _DistortFactor;	//扭曲强度
+			uniform  float4 _DistortCenter;	//扭曲中心点xy值（0-1）屏幕空间
+			sampler2D _NoiseTex;
+			float _DistortStrength;
+			
+			v2f vert (a2v v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				//计算偏移的方向
+				float2 dir = i.uv - _DistortCenter.xy;
+
+				//最终偏移的值：方向 * （1-长度），越靠外偏移越小
+				float2 scaleOffset = _DistortFactor * normalize(dir) * (1 - length(dir));
+
+				//采样Noise贴图
+				fixed4 noise = tex2D(_NoiseTex, i.uv);
+
+				//noise的权重 = 参数 * 距离，越靠近外边的部分，扰动越严重
+				float2 noiseOffset = noise.xy * _DistortStrength * dir;
+
+				//计算最终offset = 两种扭曲offset的差（取和也行，总之效果好是第一位的）
+				float2 offset = scaleOffset - noiseOffset;
+
+				//计算采样uv值：正常uv值+从中间向边缘逐渐增加的采样距离
+				float2 uv = i.uv + offset;
+				return tex2D(_MainTex, uv);
+
+			}
+			ENDCG
+		}
+	}
+}
