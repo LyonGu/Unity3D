dcb6f0f4bf88b903b0ada98adba47d205c9cf7ce
hexinping <hexinping456@126.com>
2019-04-25 20:58:04 +0800

    添加动画切换

--- UnityLearn/Assets/Scripts/StataMachine/PlayerEnitity.cs
+++ UnityLearn/Assets/Scripts/StataMachine/PlayerEnitity.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class PlayerEnitity:BaseEnitity  {
@@ -10,27 +11,175 @@ public class PlayerEnitity:BaseEnitity  {
     private GameObject _rootObj;
     public GameObject _gameObject;
 
+    private List<string> _animationNameList;
+
+    private List<BaseState> _stateList;
+
+    private Animation _animation;
+
     public PlayerEnitity()
     {
         _mode = new PlayerMode();
-        _mode._file = "Models/SwordsMan/GreateWarrior";
+        _mode._file = "Models/SwordsMan/GreateWarriorNew";
+
+        _stateList = new List<BaseState>();
+
+        
+        BaseState playerIdleState = new PlayerIdleState(this);
+        BaseState playerRunState = new PlayerRunState(this);
+        BaseState playerAttackState = new PlayerAttackState(this);
+        BaseState playerDeadState = new PlayerDeadState(this);
+
+        _stateList.Add(playerIdleState);
+        _stateList.Add(playerRunState);
+        _stateList.Add(playerAttackState);
+        _stateList.Add(playerDeadState);
 
         //状态机设置
-        BaseState state = new PlayerIdleState(this);
-        _stateMachine.setCurrentState(state);
+        //changeState(playerIdleState);
+        _stateMachine.setCurrentState(playerIdleState);
 
+        _animationNameList = new List<string>();
+        
+     
     }
     override public void initGameObject()
     {
         _rootObj = GameObject.Find("StartGame");
         if (_rootObj)
         {
-            _gameObject = getGameObject(_mode._file, "GreateWarrior", _rootObj, Vector3.zero);
+            _gameObject = getGameObject(_mode._file, "GreateWarriorNew", _rootObj, Vector3.zero);
             _gameObject.transform.localScale = new Vector3(20.0f, 20.0f, 20.0f);
             _gameObject.transform.eulerAngles = new Vector3(0.0f, 180.0f, 0);
+
+        }
+
+        //动作添加
+        if (_gameObject != null)
+        {
+            AddAinimainClips();
+            Animation animation = _gameObject.GetComponent<Animation>();
+            if(animation == null)
+            {
+                _gameObject.AddComponent<Animation>();
+                animation = _gameObject.GetComponent<Animation>();
+            }
+
+            _animation = animation;
+            foreach(string clipName in _animationNameList)
+            {
+                string path = "Models/SwordsMan/SwordsManResources/Animations/StoneKing@" + clipName;
+                AnimationClip clip = Resources.Load<AnimationClip>(path);
+                _animation.AddClip(clip, clipName);
+            }
+            changeAniamtion(_animationNameList[0], 1.0f, true);
+            //测试代码
+            addBtnListener();
         }
     }
 
 
+    public void AddAinimainClips()
+    {
+        _animationNameList.Add("Idle");
+        _animationNameList.Add("Run");
+        _animationNameList.Add("Attack2");
+        _animationNameList.Add("Death");
+    }
+
+    public void onClick(Button btn)
+    { 
+        //点击按钮切换不同的状态
+        string name = btn.name;
+        int stateIndex = 0;
+        for (int i = 0; i < _animationNameList.Count; i++)
+        {
+            if (name.Equals(_animationNameList[i]))
+            {
+                stateIndex = i;
+
+                break;
+            }
+        }
+
+
+        //切换状态
+        BaseState state = _stateList[stateIndex];
+        changeState(state);
+
+        //切换动作
+        string animatinName = name;
+        changeAniamtion(animatinName);
+        
+    }
+
+
+    public void changeAniamtion(string animatinName, float speed = 1.0f, bool isLoop = false)
+    {
+
+        /*
+         * AnimationClip:
+                frameRate  帧率
+         *      length     长度（秒）
+         *      AddEvent 添加帧事件
+         *      
+         * 
+         * AnimationState：
+         *      clip
+         *      length 长度（秒）
+         *      speed  播放倍速
+         *      time  好像可以回放 。。。不确定
+         
+         */
+        if (_animation != null)
+        {
+            //_animation.CrossFade(animatinName);
+
+            foreach (AnimationState state in _animation)
+            {
+                if (animatinName.Equals(state.name))
+                {
+                    state.speed = speed;
+                    AnimationClip clip = state.clip;
+
+                    if (isLoop)
+                    {
+                        //循环播放
+                        _animation.wrapMode = WrapMode.Loop;
+                    }
+                    else
+                    {
+                        _animation.wrapMode = WrapMode.Once;
+                    }
+
+                    
+                    _animation.CrossFade(animatinName);
+                    break;
+                }
+            }
+
+        }
+
+
+        
+    }
+
+
+    //以下是测试代码
+    public void addBtnListener()
+    {
+        GameObject canvas = GameObject.Find("Canvas");
+        //拿到该对象上（包括子对象）所有的按钮
+        Button[] btns = canvas.GetComponentsInChildren<Button>();
+
+        foreach (Button btn in btns)
+        {
+            btn.onClick.AddListener(delegate()
+            {
+                this.onClick(btn);
+            });
+        }
+
+    }
 
 }
--- UnityLearn/Assets/Scripts/StataMachine/PlayerState.cs
+++ UnityLearn/Assets/Scripts/StataMachine/PlayerState.cs
@@ -2,8 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+        //_animationNameList.Add("Idle");
+        //_animationNameList.Add("Run");
+        //_animationNameList.Add("Attack2");
+        //_animationNameList.Add("Death");
+public enum PlayerStateEnum
+{
+    IDLE            = 0,
+    RUN             = 1,
+    ATTACK          = 2,
+    DEAD            = 3,
+}
+
+
 public class PlayerState : BaseState {
 
+    public PlayerStateEnum stateIndex;
     public PlayerState(BaseEnitity enity)
         : base(enity)
     {
@@ -19,7 +35,7 @@ public class PlayerIdleState : PlayerState
 
     public PlayerIdleState(BaseEnitity enity):base(enity)
     {
-       
+        stateIndex = PlayerStateEnum.IDLE;
     }
 
     override public  void enter(params object[] values)
@@ -30,7 +46,7 @@ public class PlayerIdleState : PlayerState
 
     override public void excute(params object[] values)
     {
-        Debug.Log("玩家excute================");
+        //Debug.Log("玩家excute================");
     }
 
 
@@ -47,7 +63,7 @@ public class PlayerIdleState : PlayerState
             Dictionary<string, object> extraInfo = msg._extraInfo;
 
             string valus = (string)extraInfo["msgParams"];
-            Debug.Log("接收到消息了"+msg._dispatchTime + " 消息ID：" + msg._messageId + " 消息内容是：" + valus);
+            Debug.Log("接收到消息了" + msg._dispatchTime + " 消息ID：" + msg._messageId + " 消息内容是：" + valus);
 
             return true;
         }
@@ -55,5 +71,99 @@ public class PlayerIdleState : PlayerState
         return false;
     }
 
+}
+
+
+//玩家Run状态
+public class PlayerRunState : PlayerState
+{
+
+    public PlayerRunState(BaseEnitity enity)
+        : base(enity)
+    {
+        stateIndex = PlayerStateEnum.RUN;
+    }
+
+    override public void enter(params object[] values)
+    {
+
+
+    }
+
+    override public void excute(params object[] values)
+    {
+       
+    }
+
+
+    override public void exit(params object[] values)
+    {
+
+    }
+
 
 }
+
+
+//玩家Attack状态
+public class PlayerAttackState : PlayerState
+{
+
+    public PlayerAttackState(BaseEnitity enity)
+        : base(enity)
+    {
+        stateIndex = PlayerStateEnum.ATTACK;
+    }
+
+    override public void enter(params object[] values)
+    {
+
+
+    }
+
+    override public void excute(params object[] values)
+    {
+
+    }
+
+
+    override public void exit(params object[] values)
+    {
+
+    }
+
+
+}
+
+
+
+//玩家Dead状态
+public class PlayerDeadState : PlayerState
+{
+
+    public PlayerDeadState(BaseEnitity enity)
+        : base(enity)
+    {
+        stateIndex = PlayerStateEnum.DEAD;
+    }
+
+    override public void enter(params object[] values)
+    {
+
+
+    }
+
+    override public void excute(params object[] values)
+    {
+
+    }
+
+
+    override public void exit(params object[] values)
+    {
+
+    }
+
+
+}
+
