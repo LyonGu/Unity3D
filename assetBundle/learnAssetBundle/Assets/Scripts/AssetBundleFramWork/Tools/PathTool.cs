cf9b1e7e4795457357cd8d3b02d516ce7324f99b
hexinping <hexinping456@126.com>
2019-05-02 17:39:23 +0800

    1 自动设置ab包的名称脚本，按照“场景名+类型名”规则 2 添加删除以及打包脚本

---
+++ assetBundle/learnAssetBundle/Assets/Scripts/AssetBundleFramWork/Tools/PathTool.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathTool {
+
+	public const string AB_RESROOTPATH = "AB_Res";
+
+	//得到AB资源的输入目录
+	public static string GetABResourcesPath()
+	{
+		return Application.dataPath + "/" + AB_RESROOTPATH;
+	}
+
+	//获取AB输出路径
+	public static string GetABOutPath()
+	{
+		return GetPlatformPath() + "/" + GetPlatformName();
+	}
+
+	/// 获取平台的路径
+	private static string GetPlatformPath()
+	{
+		string strReturnPlatformPath = string.Empty;
+		switch (Application.platform)
+		{
+			case RuntimePlatform.WindowsPlayer:
+			case RuntimePlatform.WindowsEditor:
+			case RuntimePlatform.OSXEditor:
+				strReturnPlatformPath = Application.streamingAssetsPath;
+				break;
+			case RuntimePlatform.IPhonePlayer:
+			case RuntimePlatform.Android:
+				strReturnPlatformPath = Application.persistentDataPath;
+				break;
+			default:
+				break;
+		}
+
+		return strReturnPlatformPath;
+	}
+
+	/// 获取平台的名称
+	private static string GetPlatformName()
+	{
+		string strReturnPlatformName = string.Empty;
+
+		switch (Application.platform)
+		{
+			case RuntimePlatform.WindowsPlayer:
+			case RuntimePlatform.WindowsEditor:
+				strReturnPlatformName = "Windows";
+				break;
+			case RuntimePlatform.IPhonePlayer:
+				strReturnPlatformName = "Iphone";
+				break;
+			case RuntimePlatform.Android:
+				strReturnPlatformName = "Android";
+				break;
+			case RuntimePlatform.OSXEditor:
+				strReturnPlatformName = "MACOS";
+				break;
+			default:
+				break;
+		}
+
+		return strReturnPlatformName;
+	}
+
+
+
+}
