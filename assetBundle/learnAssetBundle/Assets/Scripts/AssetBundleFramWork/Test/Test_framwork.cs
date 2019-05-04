d8d3d2041b90c1c395c06ecfdc697019333284d1
hexinping <hexinping456@126.com>
2019-05-04 21:54:44 +0800

    ab框架添加非gameObject资源测试

--- assetBundle/learnAssetBundle/Assets/Scripts/AssetBundleFramWork/Test/Test_framwork.cs
+++ assetBundle/learnAssetBundle/Assets/Scripts/AssetBundleFramWork/Test/Test_framwork.cs
@@ -10,6 +10,12 @@ using UnityEngine.UI;
 
 public class Test_framwork : MonoBehaviour {
 
+    //非GameObject资源测试：texture
+    public GameObject goCubeChangeTexture1;
+    private string _assetTexBundleName = "scene_1/textures.ab";
+    private string _assetTexName = "unitychan_tile6";
+
+    //GameObject资源测试：prefab
     //场景名称
     private string _sceneName = "scene_1";
 
@@ -22,38 +28,36 @@ public class Test_framwork : MonoBehaviour {
 	void Start () {
         Debug.Log("开始测试框架=========");
        
-        //StartCoroutine(AssetBundleMgr.GetInstance().LoadAssetBundlePack(_sceneName, _assetBundleName, LoadAllABComplete)); //包加载完设置回调
-        StartCoroutine(AssetBundleMgr.GetInstance().LoadAssetBundlePack(_sceneName, _assetBundleName)); //仅仅只做包加载
-        this.Invoke("testCache",5.0f);
+        StartCoroutine(AssetBundleMgr.GetInstance().LoadAssetBundlePack(_sceneName, _assetBundleName, LoadAllABComplete)); //包加载完设置回调
+        //StartCoroutine(AssetBundleMgr.GetInstance().LoadAssetBundlePack(_sceneName, _assetBundleName)); //仅仅只做包加载
 	}
 
-    //测试同一一个ab包的缓存
-    private void testCache()
-    {
-        Object tObj = (Object)AssetBundleMgr.GetInstance().LoadAsset(_sceneName, _assetBundleName, _assetName);
-        if (tObj != null)
-        {
-            Instantiate(tObj);
-        }
 
-        //对于加载过的ab包，内存里不能再次添加，可以直接调用即可
-       // StartCoroutine(AssetBundleMgr.GetInstance().LoadAssetBundlePack(_sceneName, _assetBundleName, LoadAllABComplete1));
-        Object tObj1 = (Object)AssetBundleMgr.GetInstance().LoadAsset(_sceneName, _assetBundleName, _assetName1);
-        if (tObj1 != null)
-        {
-            Instantiate(tObj1);
-        }
-    }
 
     private void LoadAllABComplete(string abName)
     {
 
         Debug.Log("所有AB包都加载完毕=========");
-
+        //加载有依赖的ab包资源
         Object tObj = (Object)AssetBundleMgr.GetInstance().LoadAsset(_sceneName,_assetBundleName,_assetName);
         if (tObj != null)
         {
             Instantiate(tObj);
+        }
+
+        //加载没有有依赖的ab包资源
+        //对于加载过的ab包，内存里不能再次添加，可以直接调用即可
+        Object tObj1 = (Object)AssetBundleMgr.GetInstance().LoadAsset(_sceneName, _assetBundleName, _assetName1);
+        if (tObj1 != null)
+        {
+            Instantiate(tObj1);
+        }
+
+        //非gameObject资源
+        Texture tex1 = (Texture)AssetBundleMgr.GetInstance().LoadAsset(_sceneName, _assetTexBundleName, _assetTexName);
+        if (tex1 != null)
+        {
+            goCubeChangeTexture1.GetComponent<Renderer>().material.mainTexture = tex1;
         }
     }
 
