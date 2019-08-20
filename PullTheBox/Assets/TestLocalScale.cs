6b90c527c92af73603dc76eb03388b116e955192
hexinping <hexinping456@126.com>
2019-08-20 22:14:11 +0800

    pullbox 工程提交

---
+++ PullTheBox/Assets/TestLocalScale.cs
@@ -0,0 +1,50 @@
+﻿
+/***
+ *
+ *  Title: "Guardian" 项目
+ *         描述：
+ *
+ *  Description:
+ *        功能：
+ *       
+ *
+ *  Date: 2019
+ * 
+ *  Version: 1.0
+ *
+ *  Modify Recorder:
+ *     
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestLocalScale : MonoBehaviour {
+
+
+    private Transform cube;
+	// Use this for initialization
+	void Start () {
+        cube = this.transform;
+        StartCoroutine("scaleAction");
+	}
+
+    IEnumerator scaleAction()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.2f);
+            Vector3 scale = cube.localScale;
+            scale.z += 0.2f;
+            cube.localScale = scale;
+        }
+       
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
