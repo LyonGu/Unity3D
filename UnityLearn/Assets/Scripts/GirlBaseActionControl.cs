ee99d28d4f41ec91bd2f882335ce0c6a7588a89d
hexinping <hexinping456@126.com>
2019-11-27 13:56:36 +0800

    cinemachine结合timeline

--- UnityLearn/Assets/Scripts/GirlBaseActionControl.cs
+++ UnityLearn/Assets/Scripts/GirlBaseActionControl.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class GirlBaseActionControl : MonoBehaviour {
 
@@ -8,40 +9,52 @@ public class GirlBaseActionControl : MonoBehaviour {
     public float floRatitionSpeed = 1F;   //旋转速度
     private Animator AnimaObj;
     private Rigidbody myRigidbody;
+    public PlayableDirector playableDirector;
+    private Vector3 oldPosition;
+
 
     // Use this for initialization
     void Start() {
         AnimaObj = this.GetComponent<Animator>();
         myRigidbody = this.GetComponent<Rigidbody>();
-
     }
 
     // Update is called once per frame
     void Update() {
 
+        
         if (Input.GetKey(KeyCode.W))
         {
+            ResetPlaybleState();
             UpdateAnimator();
             UpdatePosAndDir(new Vector3(0, 0, 0));
         }
         else if (Input.GetKey(KeyCode.S))
         {
+            ResetPlaybleState();
             UpdateAnimator();
             UpdatePosAndDir(new Vector3(0, 180, 0));
         }
         else if (Input.GetKey(KeyCode.A))
         {
+            ResetPlaybleState();
             UpdateAnimator();
             UpdatePosAndDir(new Vector3(0, 270, 0));
         }
         else if (Input.GetKey(KeyCode.D))
         {
+            ResetPlaybleState();
             UpdateAnimator();
             UpdatePosAndDir(new Vector3(0, 90, 0));
         }
         else
         {
             UpdateAnimator(true);
+        }
+        if (playableDirector.state != PlayState.Playing)
+        {
+            oldPosition = transform.localPosition;
+            transform.localPosition = new Vector3(oldPosition.x, 0, oldPosition.z);
         }
 
     }
@@ -71,6 +84,18 @@ public class GirlBaseActionControl : MonoBehaviour {
     {
         transform.eulerAngles = vec3;
         this.transform.Translate(Vector3.forward * floWalkingSpeed * Time.deltaTime); //默认Space.Self
+        //oldPosition = transform.localPosition;
+        //transform.localPosition = new Vector3(oldPosition.x, 0, oldPosition.z);
+        //transform.localPosition = 
     }
+
+    void ResetPlaybleState()
+    {
+        if (playableDirector.state == PlayState.Playing)
+        {
+            playableDirector.Stop();
+        }
+    }
+
 
 }
