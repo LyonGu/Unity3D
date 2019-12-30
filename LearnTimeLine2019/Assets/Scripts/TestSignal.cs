9ec1f30c1e6b34e8dd713bdb21b61bc4f5234e5c
hexinping <hexinping@bytedance.com>
2019-12-30 10:26:30 +0800

    update

--- LearnTimeLine2019/Assets/Scripts/TestSignal.cs
+++ LearnTimeLine2019/Assets/Scripts/TestSignal.cs
@@ -9,4 +9,9 @@ public class TestSignal : MonoBehaviour
    {
         Debug.Log("TestRecieve============");
    }
+
+     public void TestRecieve1()
+   {
+        Debug.Log("TestRecieve1============");
+   }
 }
