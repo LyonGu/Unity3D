ea4c4e3c5f56db19a3d48420f10da5460161846d
hexinping <hexinping456@126.com>
2020-03-09 09:14:16 +0800

    1

--- Shader/Assets/Scripts/Chapter13/MotionBlurWithDepthTexture.cs
+++ Shader/Assets/Scripts/Chapter13/MotionBlurWithDepthTexture.cs
@@ -6,11 +6,11 @@ public class MotionBlurWithDepthTexture : PostEffectsBase {
 	public Shader motionBlurShader;
 	private Material motionBlurMaterial = null;
 
-	public Material material {  
+	public Material material {
 		get {
 			motionBlurMaterial = CheckShaderAndCreateMaterial(motionBlurShader, motionBlurMaterial);
 			return motionBlurMaterial;
-		}  
+		}
 	}
 
 	private Camera myCamera;
@@ -23,11 +23,11 @@ public class MotionBlurWithDepthTexture : PostEffectsBase {
 		}
 	}
 
-	[Range(0.0f, 1.0f)]
+	[Range(0.0f, 5.0f)]
 	public float blurSize = 0.5f;
 
 	private Matrix4x4 previousViewProjectionMatrix;
-	
+
 	void OnEnable() {
 
         //告诉Unity 我们要获取深度纹理
@@ -35,7 +35,7 @@ public class MotionBlurWithDepthTexture : PostEffectsBase {
 
 		previousViewProjectionMatrix = camera.projectionMatrix * camera.worldToCameraMatrix;
 	}
-	
+
 	void OnRenderImage (RenderTexture src, RenderTexture dest) {
 		if (material != null) {
 			material.SetFloat("_BlurSize", blurSize);
@@ -43,7 +43,7 @@ public class MotionBlurWithDepthTexture : PostEffectsBase {
             //传矩阵，把上一帧的矩阵记录下
 			material.SetMatrix("_PreviousViewProjectionMatrix", previousViewProjectionMatrix);
 
-            // ViewMat4 * ProjectMat4  
+            // ViewMat4 * ProjectMat4
             //camera.worldToCameraMatrix 视觉矩阵
 			Matrix4x4 currentViewProjectionMatrix = camera.projectionMatrix * camera.worldToCameraMatrix;
 			Matrix4x4 currentViewProjectionInverseMatrix = currentViewProjectionMatrix.inverse;
