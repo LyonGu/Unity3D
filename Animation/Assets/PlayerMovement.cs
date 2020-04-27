7f007a17124f79e71308498749ec0f2d230d7c16
hexinping <hexinping456@126.com>
2020-04-27 16:55:44 +0800

    Animaton工程提交

---
+++ Animation/Assets/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+
+        _animator.SetFloat("speedX", h * 2);
+        _animator.SetFloat("speedZ", v * 4);
+    }
+}
