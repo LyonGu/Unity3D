763acad55a8ed0e28f39f8e406adb6ea78090a1d
hexinping <hexinping456@126.com>
2020-04-29 18:33:03 +0800

    Unity 文件读写工程提交  添加logView插件 方便设备上查看

---
+++ LearnFileConfig/Assets/Scripts/LearnFile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LearnFile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var str = File.ReadAllText("text.txt");
+        Debug.Log(str);
+    }
+
+}
