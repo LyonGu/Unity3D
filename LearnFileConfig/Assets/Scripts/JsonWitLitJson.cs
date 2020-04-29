04b7e3a1a183144832d0c049d286c164b3e4bba9
hexinping <hexinping456@126.com>
2020-04-29 22:36:08 +0800

    【Unity读取json】 3 直接使用LitJson库 反序列化json

---
+++ LearnFileConfig/Assets/Scripts/JsonWitLitJson.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using UnityEngine;
+using LitJson;
+
+
+namespace GameLitJson
+{
+    [System.Serializable]
+    public class Npc
+    {
+        public int id;
+        public string name;
+        public float hp;
+        public float attack;
+        public float def;
+    }
+
+
+    public class JsonWitLitJson : MonoBehaviour
+    {
+        void Start()
+        {
+            var path = Utils.GetConfigFilePath("data.json");
+            var jsonStr = File.ReadAllText(path);
+
+        
+            var npcs = JsonMapper.ToObject<Npc[]>(jsonStr);
+            Debug.Log(npcs.Length);
+            foreach (var npc in npcs)
+            {
+                int id = npc.id;
+                string name = npc.name;
+                float hp = npc.hp;
+                float attack = npc.attack;
+                float def = npc.def;
+                Debug.Log("id: " + id + " name: " + name + " hp: " + hp + " attack: " + attack + " def: " + def);
+            }
+        }
+    }
+}
