44d3ccb9fd944202608f6a9c135e197085f36ca8
hexinping <hexinping456@126.com>
2020-04-30 20:01:59 +0800

    1 发布时对应dll拷贝到工程里（将里面的I18N.dll和I18N.CJK.dll复制到工程中） 2 添加拷贝配置文件脚本

---
+++ LearnFileConfig/Assets/Editor/CopyConfigFiles.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System;
+
+public class CopyConfigFiles
+{
+    // Start is called before the first frame update
+
+    [MenuItem("ConfigTools/CopyFiles")]
+    public static void CopyFiles()
+    {
+        string srcPath = Path.Combine(Application.dataPath, "Config");
+        string tarPath = Path.Combine(Application.dataPath, "StreamingAssets/Config");
+
+        CopyFolder(srcPath, tarPath);
+        AssetDatabase.Refresh();
+        Debug.Log("Copy Config Files Finished");
+    }
+
+    public static void CopyFolder(string sourcePath, string destPath)
+    {
+        if (Directory.Exists(sourcePath))
+        {
+            if (!Directory.Exists(destPath))
+            {
+                //目标目录不存在则创建
+                try
+                {
+                    Directory.CreateDirectory(destPath);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("创建目标目录失败：" + ex.Message);
+                }
+            }
+            //获得源文件下所有文件
+            List<string> files = new List<string>(Directory.GetFiles(sourcePath));
+            files.ForEach(c =>
+            {
+                string destFile = Path.Combine(new string[] { destPath, Path.GetFileName(c) });
+                File.Copy(c, destFile, true);//覆盖模式
+            });
+            //获得源文件下所有目录文件
+            List<string> folders = new List<string>(Directory.GetDirectories(sourcePath));
+            folders.ForEach(c =>
+            {
+                string destDir = Path.Combine(new string[] { destPath, Path.GetFileName(c) });
+                //采用递归的方法实现
+                CopyFolder(c, destDir);
+            });
+
+        }
+    }
+}
