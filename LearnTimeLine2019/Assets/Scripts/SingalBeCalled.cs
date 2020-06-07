ca3cd831ddb43b6320be259fc91d3c7ec2b005c1
hexinping <hexinping456@126.com>
2020-06-08 07:49:29 +0800

    【TimeLine】 使用singal attrack  ，需要在运行模式下才能看到效果，预览模式不行

---
+++ LearnTimeLine2019/Assets/Scripts/SingalBeCalled.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingalBeCalled : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void TestSingalCall(string str)
+    {
+        Debug.Log($"timeLine发射的信号接收到了=========={str}");
+    }
+}
