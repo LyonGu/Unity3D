db553e14b584c26bc4d9f132a602569f1c715734
hexinping <hexinping456@126.com>
2020-06-08 07:34:55 +0800

    【TimeLine】controlAttrack   ITimeLineControl

---
+++ LearnTimeLine2019/Assets/Scripts/LearnITimeControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.UI;
+
+public class LearnITimeControl : MonoBehaviour , ITimeControl
+{
+    // Start is called before the first frame update
+
+    public Text uitext;
+
+    private string printLabel = "timeline的controltrack运行时每一帧调用,timeline的controltrack运行时每一帧调用,timeline的controltrack运行时每一帧调用,timeline的controltrack运行时每一帧调用";
+
+    public void OnControlTimeStart()
+    {
+        Debug.Log("OnControlTimeStart======");
+        uitext.gameObject.SetActive(true);
+    }
+
+    public void OnControlTimeStop()
+    {
+        Debug.Log("OnControlTimeStop======");
+        uitext.gameObject.SetActive(false);
+    }
+
+
+    /// <summary>
+    /// timeline的control track运行时没一帧调用
+    /// </summary>
+    /// <param name="time">当前已经运行的时间</param>
+    public void SetTime(double time)
+    {
+        int len = (int)time;
+        if(len <= printLabel.Length) 
+            uitext.text = printLabel.Substring(0, len);  
+    }
+
+   
+}
