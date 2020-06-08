011434b645b2f6e18daccd8ea4bff1f30433ac8c
hexinping <hexinping456@126.com>
2020-06-08 08:39:58 +0800

    【TimeLine】 创建自定义PlayableGraph，  PlayableGraph  : 图  Playable ： 图里的资产    PlayableOutPut : 图最后的输出显示  ===》  PlayableGraph 包含Playable和PlayableOutPut

---
+++ LearnTimeLine2019/Assets/Scripts/Playable/PlayableAnimation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+[RequireComponent(typeof(Animator))]
+public class PlayableAnimation : MonoBehaviour
+{
+    public AnimationClip clip;
+    void Start()
+    {
+        /*
+            PlayableGraph  : 图
+            Playable ： 图里的资产
+            PlayableOutPut : 图最后的输出显示
+
+
+            PlayableGraph 包含Playable和PlayableOutPut
+
+         */
+        PlayableGraph graph = PlayableGraph.Create("PlayableAnimation");
+
+        AnimationClipPlayable clipPlayable = AnimationClipPlayable.Create(graph, clip);
+        AnimationPlayableOutput outPut = AnimationPlayableOutput.Create(graph, "AnimationOutPut First", GetComponent<Animator>());
+        outPut.SetSourcePlayable(clipPlayable);  //给output设置输出资产
+        graph.Play();
+    }
+
+    
+}
