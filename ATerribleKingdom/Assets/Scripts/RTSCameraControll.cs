51d6a2d5996a211227292a75b1729673bc22db15
hexinping <hexinping456@126.com>
2020-06-14 18:37:21 +0800

    timeLine和Cinimachine 练习资源

---
+++ ATerribleKingdom/Assets/Scripts/RTSCameraControll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RTSCameraControll : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform transform;
+    public float speed = 10.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform != null)
+        {
+            float h = Input.GetAxis("CameraHorizontal");
+            float v = Input.GetAxis("CameraVertical");
+            transform.position += new Vector3(h, 0, v) * (speed * Time.deltaTime);
+        }
+     
+    }
+}
