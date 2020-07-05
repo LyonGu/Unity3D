0733680e0769ef349118195d6874d36c2ebb7537
hexinping <hexinping456@126.com>
2020-07-05 12:04:04 +0800

    【Animator】 获取当前状态机所有state

---
+++ UnityLearn/Assets/GamePlayAnimator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class GamePlayAnimator : MonoBehaviour
+{
+
+
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AnimatorController animatorController = animator.runtimeAnimatorController as AnimatorController;
+        AnimatorStateMachine stateMachine = animatorController.layers[0].stateMachine;
+        string[] animatorState = new string[stateMachine.states.Length];
+        for (int i = 0; i < stateMachine.states.Length; i++)
+        {
+            animatorState[i] = stateMachine.states[i].state.name;
+            Debug.Log($"state name {animatorState[i]}");
+        }
+        //return animatorState;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
