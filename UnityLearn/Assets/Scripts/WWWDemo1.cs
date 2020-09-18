96c7dfa85e14027e0831c61cc0267018131ba8af
hexinping <hexinping456@126.com>
2020-09-18 14:18:17 +0800

    1

--- UnityLearn/Assets/Scripts/WWWDemo1.cs
+++ UnityLearn/Assets/Scripts/WWWDemo1.cs
@@ -17,21 +17,60 @@
  */
 using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class WWWDemo1 : MonoBehaviour{
     public GameObject goCube;                              //立方体
     public GameObject goSphere;                            //球体
+    public RawImage rawImage;
     private Texture2D TxtDownloadTextures;                 //需要下载的贴图
 
 
     //下载本机贴图
     public void DownLoadTexturesByWWW(){
         StartCoroutine("StartDownLoadTexture");
+
+        Sprite t;
+        RawImage ts;
     }
 
+    private void DownHttpCompleteHandle(AsyncOperation t)
+    {
+        if (t.isDone)
+        {
+            var Operation = (UnityWebRequestAsyncOperation)t;
+            var request = Operation.webRequest;
+            var myTexture = DownloadHandlerTexture.GetContent(request);
+            rawImage.texture = myTexture;
+        }
+    }
+    //Action<AsyncOperation> completed
     //从互联网下载贴图
     public void DownLoadTexturesFromHTTP(){
-        StartCoroutine("StartDownLoadTextureFromHTTP");
+
+        //WWW www = new WWW("http://imageserver.uniregistry.com/catimg/computersinternetdownloads/c3.jpg");
+        //while (!www.isDone) { }
+        //goSphere.GetComponent<Renderer>().material.mainTexture = www.texture;
+        //StartCoroutine("StartDownLoadTextureFromHTTP");
+
+        //StartCoroutine(GetTexture());
+
+        string url = "http://imageserver.uniregistry.com/catimg/computersinternetdownloads/c3.jpg";
+        UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(url);
+        {
+            uwr.SendWebRequest().completed += delegate (AsyncOperation t)
+            {
+                if (t.isDone)
+                {
+                    Debug.Log("GetURLImage done===========");
+                    var myTexture = DownloadHandlerTexture.GetContent(uwr);
+                    rawImage.texture = myTexture;
+                }
+            };
+        }
+        
+
     }
 
     //下载本机资源
@@ -56,7 +95,16 @@ public class WWWDemo1 : MonoBehaviour{
         //下载的贴图直接赋值指定的游戏对象
         goSphere.GetComponent<Renderer>().material.mainTexture = loadloadTexture.texture;
     }
-	
+
+
+    IEnumerator GetTexture()
+    {
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture("http://imageserver.uniregistry.com/catimg/computersinternetdownloads/c3.jpg");
+        yield return www.SendWebRequest();
+
+        Texture myTexture = DownloadHandlerTexture.GetContent(www);
+        rawImage.texture = myTexture;
+    }
 }
 
 
