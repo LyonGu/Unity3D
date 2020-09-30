6b2f19c4d433e2707f06c6878f9bd7eb7b9a9750
hexinping <hexinping456@126.com>
2020-09-30 18:49:12 +0800

    【Shader自定义GUI】 方法1：通过写cs脚本继承shaderGUI类实现 2 通过Unity内置的材质面板属性  推荐1，但是2真的是方便

---
+++ Shader/Assets/Scripts/CustomShaderGUI/CustomShaderGUI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+
+//必须继承ShaderGUI，然后重载OnGUI方法
+public class CustomShaderGUI : ShaderGUI
+{
+    public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
+    {
+        // render the default gui 先默认渲染一遍
+        base.OnGUI(materialEditor, properties);
+
+        Material targetMat = materialEditor.target as Material;
+
+        // see if redify is set, and show a checkbox
+        bool CS_BOOL = Array.IndexOf(targetMat.shaderKeywords, "CS_BOOL") != -1;
+
+        EditorGUI.BeginChangeCheck();
+        CS_BOOL = EditorGUILayout.Toggle("CS_BOOL", CS_BOOL);
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            // enable or disable the keyword based on checkbox
+            if (CS_BOOL)
+                //启用变体CS_BOOL
+                targetMat.EnableKeyword("CS_BOOL");
+            else
+                //禁用变体CS_BOOL
+                targetMat.DisableKeyword("CS_BOOL");
+        }
+    }
+}
