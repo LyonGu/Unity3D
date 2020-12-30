94eed6eefcce5ed40a587158d00f698f295adf85
hexinping <hexinping456@126.com>
2020-12-30 17:54:38 +0800

    ab 加载图集测试 跟一搬加载ab一样，加载图集的ab，然后直接加载sprite就行

--- assetBundle/learnAssetBundle/Assets/Scripts/AssetBundleFramWork/ABLoadAsync.cs
+++ assetBundle/learnAssetBundle/Assets/Scripts/AssetBundleFramWork/ABLoadAsync.cs
@@ -9,23 +9,41 @@ public class ABLoadAsync : MonoBehaviour
     // Start is called before the first frame update
 
     public RawImage rawimage;
+
+    public Image image;
     
     void Start()
     {
         AssetBundleCreateRequest request = AssetBundle.LoadFromFileAsync(Path.Combine(Application.streamingAssetsPath, "Windows/scene_1/textures.ab"));
-        request.completed -= abLoadDone;
-        request.completed += abLoadDone;
+        request.completed -= abTextureLoadDone;
+        request.completed += abTextureLoadDone;
+
+        AssetBundleCreateRequest request1 = AssetBundle.LoadFromFileAsync(Path.Combine(Application.streamingAssetsPath, "Windows/scene_1/atlas.ab"));
+        request1.completed -= abSpriteLoadDone;
+        request1.completed += abSpriteLoadDone;
     }
 
-    void abLoadDone(AsyncOperation t)
+    void abTextureLoadDone(AsyncOperation t)
     {
         if (t.isDone)
         {
             AssetBundleCreateRequest abRequset = t as AssetBundleCreateRequest;
             var assetBundle = abRequset.assetBundle;
 
-            Texture2D tex = assetBundle.LoadAsset<Texture2D>("WhileFloor.jpg"); // 只使用资源名就行
+            Texture2D tex = assetBundle.LoadAsset<Texture2D>("WhileFloor"); // 只使用资源名就行
             rawimage.texture = tex;
+        }
+    }
+
+    void abSpriteLoadDone(AsyncOperation t)
+    {
+        if (t.isDone)
+        {
+            AssetBundleCreateRequest abRequset = t as AssetBundleCreateRequest;
+            var assetBundle = abRequset.assetBundle;
+
+            Sprite sp = assetBundle.LoadAsset<Sprite>("FloorAl"); // 只使用资源名就行
+            image.sprite = sp;
         }
     }
 
