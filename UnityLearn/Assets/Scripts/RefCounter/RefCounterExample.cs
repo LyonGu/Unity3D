742b3ab273061baebab0c8b215c1019ceddf0df5
hexinping <hexinping456@126.com>
2021-02-15 23:00:44 +0800

    引用计数

---
+++ UnityLearn/Assets/Scripts/RefCounter/RefCounterExample.cs
@@ -0,0 +1,57 @@
+﻿
+using UnityEngine;
+
+public class RefCounterExample : MonoBehaviour
+{
+    void Start()
+    {
+        var room = new Room();
+
+        room.EnterPeople();
+        room.EnterPeople();
+        room.EnterPeople();
+
+        room.LeavePeople();
+        room.LeavePeople();
+        room.LeavePeople();
+    }
+}
+
+public class LightTest
+{
+    public void SwitchOn()
+    {
+        Debug.Log("开灯");
+    }
+
+    public void SwitchOff()
+    {
+        Debug.Log("关灯");
+    }
+}
+
+public class Room : SimpleRC
+{
+    private LightTest mLight = new LightTest();
+
+    public void EnterPeople()
+    {
+        Debug.Log("进入人了");
+        Retain();
+        if (RefCount == 1)
+        {
+            mLight.SwitchOn();
+        }
+    }
+
+    public void LeavePeople()
+    {
+        Debug.Log("人出来了");
+        Release();
+    }
+
+    protected override void OnZeroRef()
+    {
+        mLight.SwitchOff();
+    }
+}
