4cfe4fb6bcab48a2b81e53f7d7f1fdf8f6180f15
hexinping <hexinping456@126.com>
2021-02-16 22:58:53 +0800

    安全对象池

---
+++ UnityLearn/Assets/Scripts/Pool/IPoolable.cs
@@ -0,0 +1,8 @@
+﻿/// <summary>
+/// I pool able.
+/// </summary>
+public interface IPoolable
+{
+    void OnRecycled();
+    bool IsRecycled { get; set; }  //标记对象是否被回收
+}
---
+++ UnityLearn/Assets/Scripts/Pool/SafeObjectPoolExample.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class Msg : IPoolable
+{
+    public void OnRecycled()
+    {
+        Debug.Log("OnRecycled");
+    }
+
+    public bool IsRecycled { get; set; }
+}
+
+public class SafeObjectPoolExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var msgPool = new SafeObjectPool<Msg>();
+
+        msgPool.Init(100, 50); // max count:100 init count: 50
+
+        Debug.Log($"msgPool.CurCount:{msgPool.CurCount}");
+
+        var fishOne = msgPool.Allocate();
+
+        Debug.Log($"msgPool.CurCount:{msgPool.CurCount}");
+
+        msgPool.Recycle(fishOne);
+
+        Debug.Log($"msgPool.CurCount:{msgPool.CurCount}");
+
+        for (int i = 0; i < 10; i++)
+        {
+            msgPool.Allocate();
+        }
+
+        Debug.Log($"msgPool.CurCount:{msgPool.CurCount}");
+    }
+
+    // Update is called once per frame
+   
+}
