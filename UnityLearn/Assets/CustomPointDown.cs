ff0b4bdbb4bf7e170d8ec1c466557155e4cbe1ec
hexinping <hexinping456@126.com>
2021-02-18 21:55:03 +0800

    1

---
+++ UnityLearn/Assets/CustomPointDown.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CustomPointDown : MonoBehaviour, IPointerDownHandler , ICanvasRaycastFilter
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("CustomPointDown OnPointerDown=========== ");
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool IsRaycastLocationValid(Vector2 screenPoint, Camera eventCamera)
+    {
+        bool isContain = RectTransformUtility.RectangleContainsScreenPoint(this.transform as RectTransform, screenPoint, eventCamera);
+        Debug.Log($"CustomPointDown IsRaycastLocationValid {isContain}=========== ");
+        return isContain;
+    }
+}
