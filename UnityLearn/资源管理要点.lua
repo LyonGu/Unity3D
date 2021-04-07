65fd253aae602952de4cdd8e0fc04b082aa0237c
hexinping <hexinping456@126.com>
2021-04-07 22:19:07 +0800

    update

--- UnityLearn/资源管理要点.lua
+++ UnityLearn/资源管理要点.lua
@@ -1,22 +1,22 @@
 
 1 统一的接口实现
 {
-	1 支持同步和异步
-	2 支持不同目录： Resources/AssetBundle/PersistentDataPath/StreamingAssets，甚至是网络资源
+    1 支持同步和异步
+    2 支持不同目录： Resources/AssetBundle/PersistentDataPath/StreamingAssets，甚至是网络资源
 }
 
 2 需要考虑的问题
 {
-	让我们把 资源引用计数 和 异步加载 结合起来看，异步加载 分三个阶段：未开始、进行中、已完成。异步加载一个资源，发现资源一共可能是三种状态：
+    让我们把 资源引用计数 和 异步加载 结合起来看，异步加载 分三个阶段：未开始、进行中、已完成。异步加载一个资源，发现资源一共可能是三种状态：
 
-	未开始： 开始加载资源，状态切换到进行中，设置资源引用计数，设置加载完成后回调。
-	进行中： 设置资源引用计数，设置加载完成后回调。
-	已完成： 设置资源引用计数，然后直接返回该资源。
-	资源管理还涉及到卸载，这里我们使问题简单化，卸载操作是“同步”的：首先让一个资源和对象解除关联，如果一个资源所有的引用都解除了，那么就可以卸载这个资源了。卸载时也会遇到资源状态问题：
+    未开始： 开始加载资源，状态切换到进行中，设置资源引用计数，设置加载完成后回调。
+    进行中： 设置资源引用计数，设置加载完成后回调。
+    已完成： 设置资源引用计数，然后直接返回该资源。
+    资源管理还涉及到卸载，这里我们使问题简单化，卸载操作是“同步”的：首先让一个资源和对象解除关联，如果一个资源所有的引用都解除了，那么就可以卸载这个资源了。卸载时也会遇到资源状态问题：
 
-	未开始： 报错，不能卸载未加载的资源。
-	进行中： 解除对象对资源的引用，加载完成逻辑如果发现资源已经没对象引用了，需要卸载。
-	已完成： 解除对象对资源的引用，如果资源没有对象引用，卸载。
+    未开始： 报错，不能卸载未加载的资源。
+    进行中： 解除对象对资源的引用，加载完成逻辑如果发现资源已经没对象引用了，需要卸载。
+    已完成： 解除对象对资源的引用，如果资源没有对象引用，卸载。
 }
 
 3 AssetBundle加载支持不传入ab名，只需要传入资源名即可==》资源不能同名
@@ -37,7 +37,7 @@
 
         ResSearchKeys <--IPoolable,IPoolType  
         {
-			其实就是一个搜索匹配规则的类
+            其实就是一个搜索匹配规则的类
         }
 
         IRes <-- IRefCounter, IPoolType, IEnumeratorTask
@@ -54,50 +54,72 @@
         }
 
         ResourcesRes <-- Res
-        AssetBundleRes <-- Res
-        AssetRes <-- Res
+        AssetBundleRes <-- Res ==》 下载AB使用
+        AssetRes <-- Res =》 AB里的asset
         NetImageRes <-- Res
 
+        IResDatas
+        ResDatas <-- IResDatas
 
         ResMgr
         {
-	        [SerializeField] private int                         mCurrentCoroutineCount;
-	        private                  int                         mMaxCoroutineCount    = 8; //最快协成大概在6到8之间
+            [SerializeField] private int                         mCurrentCoroutineCount;
+            private                  int                         mMaxCoroutineCount    = 8; //最快协成大概在6到8之间
 
-	        //双向列表
-	        {
-				数组和List、ArrayList集合都有一个重大的缺陷，就是从数组的中间位置删除或插入一个元素需要付出很大的代价，其原因是数组中处于被删除元素之后的所有元素都要向数组的前端移动。
-				LinkedList（底层是由链表实现的）基于链表的数据结构，很好的解决了数组删除插入效率低的问题，且不用动态的扩充数组的长度。
-				LinkedList的优点：插入、删除元素效率比较高；缺点：访问效率比较低。
+            //双向列表
+            {
+                数组和List、ArrayList集合都有一个重大的缺陷，就是从数组的中间位置删除或插入一个元素需要付出很大的代价，其原因是数组中处于被删除元素之后的所有元素都要向数组的前端移动。
+                LinkedList（底层是由链表实现的）基于链表的数据结构，很好的解决了数组删除插入效率低的问题，且不用动态的扩充数组的长度。
+                LinkedList的优点：插入、删除元素效率比较高；缺点：访问效率比较低。
 
-				LinkedList< T>是一个双向链表 – 每个节点都知道它的前一个条目和下一个条目。这对于在特定节点(或头/尾)之后/之后插入是快速的，但是通过索引访问缓慢。
+                LinkedList< T>是一个双向链表 – 每个节点都知道它的前一个条目和下一个条目。这对于在特定节点(或头/尾)之后/之后插入是快速的，但是通过索引访问缓慢。
 
-				LinkedList< T>通常会比List< T>占用更多的内存因为它需要所有下一个/先前引用的空间 – 并且数据可能具有较少的引用位置，因为每个节点是单独的对象。另一方面，列表< T>可以有一个比当前需要大得多的后备阵列。
+                LinkedList< T>通常会比List< T>占用更多的内存因为它需要所有下一个/先前引用的空间 – 并且数据可能具有较少的引用位置，因为每个节点是单独的对象。另一方面，列表< T>可以有一个比当前需要大得多的后备阵列。
 
-	        }
-	        private                  LinkedList<IEnumeratorTask> mIEnumeratorTaskStack = new LinkedList<IEnumeratorTask>();
+            }
+            private                  LinkedList<IEnumeratorTask> mIEnumeratorTaskStack = new LinkedList<IEnumeratorTask>();
 
-			ResTable
+            ResTable
 
         }
 
 
         ResFactory
         {
-			IResCreator
-			
-			ResourcesResCreator <--IResCreator
-			{
-				里面会调用ResourcesRes.Allocate
-			}
-			AssetBundleResCreator <--IResCreator
-			AssetResCreator <--IResCreator
-			AssetBundleSceneResCreator <--IResCreator
-			NetImageResCreator <--IResCreator
-			LocalImageResCreator <--IResCreator
+            IResCreator
+            
+            ResourcesResCreator <--IResCreator
+            {
+                里面会调用ResourcesRes.Allocate
+            }
+            AssetBundleResCreator <--IResCreator
+            AssetResCreator <--IResCreator
+            AssetBundleSceneResCreator <--IResCreator
+            NetImageResCreator <--IResCreator
+            LocalImageResCreator <--IResCreator
         }
 
     }
+
+    框架1： 1 每个loader有自己的list存储IRes对象 ResMgr有全局容器管理IRes对象  2 每个IRes对象有自己的加载状态 3 同步加载时放到一个等待列表里去加载
+
+     Unity asset 类型
+     {
+        public static Type GameObjectType  = typeof(GameObject);
+        public static Type AudioClipType   = typeof(AudioClip);
+        public static Type SpriteType      = typeof(Sprite);
+        public static Type SceneType       = typeof(Scene);
+        public static Type SpriteAtlasType = typeof(SpriteAtlas);
+        public static Type MeshType        = typeof(Mesh);
+        public static Type Texture2DType   = typeof(Texture2D);
+        public static Type TextAssetType   = typeof(TextAsset);
+        public static Type AssetBundleType   = typeof(AssetBundle);
+
+        animatorController
+        Animation
+        TimeLine
+        
+     }
     }
 
 ]==]
