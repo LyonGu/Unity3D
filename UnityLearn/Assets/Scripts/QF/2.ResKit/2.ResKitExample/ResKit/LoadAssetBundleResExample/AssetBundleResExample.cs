a64cd45a692500431beb132a2836361ba68d4e11
hexinping <hexinping456@126.com>
2021-04-13 17:06:07 +0800

    【ResKit】异步加载例子

--- UnityLearn/Assets/Scripts/QF/2.ResKit/2.ResKitExample/ResKit/LoadAssetBundleResExample/AssetBundleResExample.cs
+++ UnityLearn/Assets/Scripts/QF/2.ResKit/2.ResKitExample/ResKit/LoadAssetBundleResExample/AssetBundleResExample.cs
@@ -6,8 +6,9 @@ namespace QFramework.Example
 	public class AssetBundleResExample : MonoBehaviour
 	{
 		private ResLoader mResLoader = ResLoader.Allocate();
+        private ResLoader mResLoaderAsync = ResLoader.Allocate();
 
-		public RawImage RawImage;
+        public RawImage RawImage;
 
 		private void Awake()
 		{
@@ -20,11 +21,29 @@ namespace QFramework.Example
 			RawImage rawImage = transform.Find("RawImage").GetComponent<RawImage>();
 
             // resource资源加载
-			//RawImage.texture = mResLoader.LoadSync<Texture2D>("TestImage");
-			
-			// ab资源加载通过下边方式也一样
-			RawImage.texture = mResLoader.LoadSync<Texture2D>("testimage_png","TestImage");
-		}
+            //RawImage.texture = mResLoader.LoadSync<Texture2D>("TestImage");
+
+            // ab资源加载通过下边方式也一样
+            //RawImage.texture = mResLoader.LoadSync<Texture2D>("testimage_png","TestImage");
+
+            //mResLoader.LoadAsync();
+            //mResLoader.LoadAsync
+
+            //mResLoader.LoadSync()
+            mResLoader.Add2Load<Texture2D>("TestImage", (succeed, res) =>
+            {
+                Debug.Log($"succeed========={succeed}");
+                if (succeed)
+                {
+                    Texture2D texture = res.Asset as Texture2D;
+                    RawImage.texture = texture;
+                }
+            });
+            mResLoader.LoadAsync(()=> {
+                Debug.Log("资源加载完毕=======");
+            });
+
+        }
 
 		private void OnDestroy()
 		{
--- UnityLearn/Assets/Scripts/QF/2.ResKit/Runtime/AssetBundleSupport/ResCreator/AssetResCreator.cs
+++ UnityLearn/Assets/Scripts/QF/2.ResKit/Runtime/AssetBundleSupport/ResCreator/AssetResCreator.cs
@@ -4,7 +4,7 @@ namespace QFramework
     {
         public bool Match(ResSearchKeys resSearchKeys)
         {
-            var assetData =  AssetBundleSettings.AssetBundleConfigFile.GetAssetData(resSearchKeys);
+            var assetData =  AssetBundleSettings.AssetBundleConfigFile.GetAssetData(resSearchKeys); // 通过去一个ab的配置里找，记录了工程里使用的所有的ab配置
 
             if (assetData != null)
             {
