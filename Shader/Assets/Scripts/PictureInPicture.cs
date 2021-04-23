df0b5c90cbe1e86226b08dd5ff5d86ff61718791
hexinping <hexinping456@126.com>
2021-04-23 13:10:09 +0800

    画中画Shader

---
+++ Shader/Assets/Scripts/PictureInPicture.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PictureInPicture : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public RawImage rawImage;
+
+    private void OnEnable()
+    {
+        var material = rawImage.material;
+        material.SetTexture("_MainTex", rawImage.texture);
+    }
+
+    // Update is called once per frame
+
+}
