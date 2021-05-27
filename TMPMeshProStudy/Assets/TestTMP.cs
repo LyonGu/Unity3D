e099153dcf42575ae9c717c44cb00a097cacd957
hexinping <hexinping456@126.com>
2021-05-27 19:04:38 +0800

    tmp Update

---
+++ TMPMeshProStudy/Assets/TestTMP.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTMP : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject testPrefab;
+    public Transform parentTranform;
+    void Start()
+    {
+        StartCoroutine(CreateTMP());
+    }
+
+
+    IEnumerator CreateTMP()
+    {
+        yield return new WaitForSeconds(5);
+        GameObject t = GameObject.Instantiate(testPrefab);
+        t.transform.SetParent(parentTranform, false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
