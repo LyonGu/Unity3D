bf24f5b932491e559eeef10528395fbaeab4becd
hexinping <hexinping456@126.com>
2021-06-15 21:28:12 +0800

    Build New Project

---
+++ TMPMestProStudy/Assets/TestTMP.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTMP : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform parentTrans;
+    
+    void Start()
+    {
+        StartCoroutine("TestTmp");
+    }
+
+    IEnumerator TestTmp()
+    {
+        yield return null;
+
+        yield return new WaitForSeconds(3);
+
+        GameObject gameObject = Resources.Load<GameObject>("TMPGameObject");
+        var targetObj = GameObject.Instantiate<GameObject>(gameObject);
+        targetObj.transform.SetParent(parentTrans, false);
+    }
+
+    // Update is called once per frame
+   
+}
