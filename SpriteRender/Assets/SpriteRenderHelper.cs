5ef38d8b6932ac0a338c20f4f6eb59d1066ec5ef
hexinping <hexinping456@126.com>
2021-08-03 19:40:16 +0800

    SpriteRender Update

---
+++ SpriteRender/Assets/SpriteRenderHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteRenderHelper : MonoBehaviour
+{
+
+    public SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer.lightProbeUsage = 0;
+        spriteRenderer.reflectionProbeUsage = 0;
+    }
+
+}
