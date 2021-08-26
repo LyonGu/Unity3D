86fb1fe536f23bea52b9a9b2dcd2a00124efefd1
hexinping <hexinping456@126.com>
2021-08-26 10:07:04 +0800

    【ECSLearn】 FindTarget 工程提交 优化TODO

---
+++ ECSLearn/Assets/ECS_FindTarget/UnitMoveToTargetSystem.cs
@@ -0,0 +1,45 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Transforms;
+using Unity.Entities;
+using Unity.Mathematics;
+
+public class UnitMoveToTargetSystem : ComponentSystem {
+
+    protected override void OnUpdate()
+    {
+        float DeltaTime = Time.DeltaTime;
+        Entities.ForEach((Entity unitEntity, ref HasTarget hasTarget, ref Translation translation) => {
+            if (World.DefaultGameObjectInjectionWorld.EntityManager.Exists(hasTarget.targetEntity)) {
+                Translation targetTranslation = World.DefaultGameObjectInjectionWorld.EntityManager.GetComponentData<Translation>(hasTarget.targetEntity);
+
+                float3 targetDir = math.normalize(targetTranslation.Value - translation.Value);
+                float moveSpeed = 5f;
+                translation.Value += targetDir * moveSpeed * DeltaTime;
+
+                if (math.distance(translation.Value, targetTranslation.Value) < .2f) {
+                    // Close to target, destroy it
+                    PostUpdateCommands.DestroyEntity(hasTarget.targetEntity);
+                    PostUpdateCommands.RemoveComponent(unitEntity, typeof(HasTarget));
+                }
+            } else {
+                // Target Entity already destroyed
+                PostUpdateCommands.RemoveComponent(unitEntity, typeof(HasTarget));
+            }
+        });
+    }
+
+}
