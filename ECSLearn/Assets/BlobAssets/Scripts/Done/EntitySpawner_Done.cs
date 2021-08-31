9c202c7b1bf4af65ad6d06aeaad3d90f8391e012
hexinping <hexinping456@126.com>
2021-08-31 14:11:56 +0800

    Update

--- ECSLearn/Assets/BlobAssets/Scripts/Done/EntitySpawner_Done.cs
+++ ECSLearn/Assets/BlobAssets/Scripts/Done/EntitySpawner_Done.cs
@@ -10,22 +10,22 @@ public class EntitySpawner_Done : JobComponentSystem {
     private float spawnTimerMax = .15f;
 
     protected override JobHandle OnUpdate(JobHandle inputDeps) {
-        if (spawnCount > 0) {
-            spawnTimer -= Time.DeltaTime;
-
-            if (spawnTimer <= 0f) {
-                spawnTimer += spawnTimerMax;
-                spawnCount--;
-                PrefabEntityHolder prefabEntityHolder = GetSingleton<PrefabEntityHolder>();
-
-                Entity spawnedEntity = EntityManager.Instantiate(prefabEntityHolder.entityPrefab);
-
-                EntityManager.AddComponentData(spawnedEntity, new WaypointFollow_Done {
-                    waypointBlobAssetReference = WaypointBlobAssetConstructor_Done.blobAssetReference,
-                    waypointIndex = 0,
-                });
-            }
-        }
+//        if (spawnCount > 0) {
+//            spawnTimer -= Time.DeltaTime;
+//
+//            if (spawnTimer <= 0f) {
+//                spawnTimer += spawnTimerMax;
+//                spawnCount--;
+//                PrefabEntityHolder prefabEntityHolder = GetSingleton<PrefabEntityHolder>();
+//
+//                Entity spawnedEntity = EntityManager.Instantiate(prefabEntityHolder.entityPrefab);
+//
+//                EntityManager.AddComponentData(spawnedEntity, new WaypointFollow_Done {
+//                    waypointBlobAssetReference = WaypointBlobAssetConstructor_Done.blobAssetReference,
+//                    waypointIndex = 0,
+//                });
+//            }
+//        }
 
         return default;
     }
--- ECSLearn/Assets/BlobAssets/Scripts/Done/WaypointBlobAssetConstructor_Done.cs
+++ ECSLearn/Assets/BlobAssets/Scripts/Done/WaypointBlobAssetConstructor_Done.cs
@@ -11,25 +11,25 @@ public class WaypointBlobAssetConstructor_Done : GameObjectConversionSystem {
     public static BlobAssetReference<WaypointBlobAsset_Done> blobAssetReference;
 
     protected override void OnUpdate() {
-        using (BlobBuilder blobBuilder = new BlobBuilder(Allocator.Temp)) {
-            ref WaypointBlobAsset_Done waypointBlobAsset = ref blobBuilder.ConstructRoot<WaypointBlobAsset_Done>();
-            
-            WaypointBlobAssetAuthoring_Done waypointAuthoring = GetEntityQuery(typeof(WaypointBlobAssetAuthoring_Done)).ToComponentArray<WaypointBlobAssetAuthoring_Done>()[0];
-
-            BlobBuilderArray<Waypoint_Done> waypointBlobBuilderArray = blobBuilder.Allocate(ref waypointBlobAsset.waypointArray, waypointAuthoring.transformArray.Length);
-
-            for (int i = 0; i < waypointAuthoring.transformArray.Length; i++) { 
-                Transform waypointTransform = waypointAuthoring.transformArray[i];
-                waypointBlobBuilderArray[i] = new Waypoint_Done { position = waypointTransform.position };
-            }
-
-            //blobBuilder.AllocateString(ref waypointBlobAsset.blobString, "Test String!");
-
-            ref Waypoint_Done waypoint = ref blobBuilder.Allocate(ref waypointBlobAsset.singleWaypoint);
-            waypoint = new Waypoint_Done { position = new float3(56, 0, 0) };
-
-            blobAssetReference = blobBuilder.CreateBlobAssetReference<WaypointBlobAsset_Done>(Allocator.Persistent);
-        }
+//        using (BlobBuilder blobBuilder = new BlobBuilder(Allocator.Temp)) {
+//            ref WaypointBlobAsset_Done waypointBlobAsset = ref blobBuilder.ConstructRoot<WaypointBlobAsset_Done>();
+//            
+//            WaypointBlobAssetAuthoring_Done waypointAuthoring = GetEntityQuery(typeof(WaypointBlobAssetAuthoring_Done)).ToComponentArray<WaypointBlobAssetAuthoring_Done>()[0];
+//
+//            BlobBuilderArray<Waypoint_Done> waypointBlobBuilderArray = blobBuilder.Allocate(ref waypointBlobAsset.waypointArray, waypointAuthoring.transformArray.Length);
+//
+//            for (int i = 0; i < waypointAuthoring.transformArray.Length; i++) { 
+//                Transform waypointTransform = waypointAuthoring.transformArray[i];
+//                waypointBlobBuilderArray[i] = new Waypoint_Done { position = waypointTransform.position };
+//            }
+//
+//            //blobBuilder.AllocateString(ref waypointBlobAsset.blobString, "Test String!");
+//
+//            ref Waypoint_Done waypoint = ref blobBuilder.Allocate(ref waypointBlobAsset.singleWaypoint);
+//            waypoint = new Waypoint_Done { position = new float3(56, 0, 0) };
+//
+//            blobAssetReference = blobBuilder.CreateBlobAssetReference<WaypointBlobAsset_Done>(Allocator.Persistent);
+//        }
     }
 
 }
