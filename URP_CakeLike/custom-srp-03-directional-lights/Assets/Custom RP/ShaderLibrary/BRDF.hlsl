b8ff9c1b561645146d3c46f26c88bb5caf44a8f3
hexinping <hexinping456@126.com>
2021-09-19 07:16:01 +0800

    【URP】 3 方向光（Direct Illumination）

--- URP_CakeLike/custom-srp-03-directional-lights/Assets/Custom RP/ShaderLibrary/BRDF.hlsl
+++ URP_CakeLike/custom-srp-03-directional-lights/Assets/Custom RP/ShaderLibrary/BRDF.hlsl
@@ -9,6 +9,14 @@ struct BRDF {
 
 #define MIN_REFLECTIVITY 0.04
 
+/*
+不同的表面，反射的方式不同，但通常金属会通过镜面反射反射所有光，并且漫反射为零。因此，我们将声明反射率等于金属表面属性
+    float oneMinusReflectivity = 1.0 - surface.metallic;
+	brdf.diffuse = surface.color * oneMinusReflectivity;
+	
+	非金属的反射率有所不同，但平均约为0.04。让我们将其定义为最小反射率，并添加一个OneMinusReflectivity函数，
+	该函数将范围从0~1调整为0~0.96。此范围调整与Universal RP的方法匹配
+*/
 float OneMinusReflectivity (float metallic) {
 	float range = 1.0 - MIN_REFLECTIVITY;
 	return range - metallic * range;
@@ -17,11 +25,17 @@ float OneMinusReflectivity (float metallic) {
 BRDF GetBRDF (Surface surface, bool applyAlphaToDiffuse = false) {
 	BRDF brdf;
 	float oneMinusReflectivity = OneMinusReflectivity(surface.metallic);
-
+    //散射的光 surface.color其实就是Albedo
+    /*
+        材质的漫反射率的颜色称为反照率，可以使用材质的纹理和色调来定义它
+		Albedo ==> 反照率 
+		float3 albedo = tex2D(_MainTex, i.uv).rgb * _Tint.rgb;
+    */
 	brdf.diffuse = surface.color * oneMinusReflectivity;
 	if (applyAlphaToDiffuse) {
 		brdf.diffuse *= surface.alpha;
 	}
+	//反射的光 surface.color其实就是Albedo
 	brdf.specular = lerp(MIN_REFLECTIVITY, surface.color, surface.metallic);
 
 	float perceptualRoughness =
