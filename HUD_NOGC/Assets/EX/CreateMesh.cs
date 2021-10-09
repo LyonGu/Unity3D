17e0fb886ca9e4c64f99e610126aac813d3abf96
hexinping <hexinping456@126.com>
2021-10-09 12:18:45 +0800

    【HUD_NOGC】 使用顶点位置改变显示区域

--- HUD_NOGC/Assets/EX/CreateMesh.cs
+++ HUD_NOGC/Assets/EX/CreateMesh.cs
@@ -10,6 +10,8 @@ public class CreateMesh : MonoBehaviour
     public MeshFilter meshFilter;
     public Material material;
     private Mesh _mesh;
+    [Range(0,1)]
+    public float progress = 1.0f;
     
     //偏移量
     public float OffsetY;
@@ -30,23 +32,27 @@ public class CreateMesh : MonoBehaviour
 
     private void InitMesh()
     {
-        
+
         //顶点信息 注意原点在中间的位置
         //当前位置乘以Matrix4x4.identity后，这里的顶点信息 最后参与计算，
         //其实可以用顶点数组来控制位置
+        float vertProgress = progress * 2 -1;
         Vector3[] vec = new Vector3[4]
         {
-            new Vector3(1 + OffsetX,1+ OffsetY,0), //右上 0
-            new Vector3(1 + OffsetX,-1+ OffsetY,0), //右下 1
+            new Vector3(1* vertProgress + OffsetX,1+ OffsetY,0), //右上 0
+            new Vector3(1* vertProgress+ OffsetX,-1+ OffsetY,0), //右下 1
             new Vector3(-1+ OffsetX,-1+ OffsetY,0), //左下 2
             new Vector3(-1+ OffsetX,1+ OffsetY,0) //左上 3
         };
+
+
         _mesh.vertices = vec;
+
         
         //UV信息 跟 顶点信息 一一对应
         Vector2[] uv = new Vector2[4]{
-            new Vector2(1, 1),
-            new Vector2(1, 0),
+            new Vector2(1.0f * progress, 1),
+            new Vector2(1.0f * progress, 0),
             new Vector2(0, 0),
             new Vector2(0, 1)
         };
