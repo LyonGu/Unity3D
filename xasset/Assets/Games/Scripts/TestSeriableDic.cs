6a86e1030b00e2008a72abc29cdf993683432580
hexinping <hexinping456@126.com>
2021-10-13 14:08:19 +0800

    SerializedDictionary 可序列化的字典，不能在编辑器里直接添加需要代码添加，需要实现 ISerializationCallbackReceiver

---
+++ xasset/Assets/Games/Scripts/TestSeriableDic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using HxpGame;
+using UnityEngine;
+
+
+
+public class TestSeriableDic : MonoBehaviour
+{
+    [Serializable]
+    public class MyData
+    {
+        public string key;
+        public string name;
+    }
+    
+    [Serializable]
+    public  class MyDictionary : SerializedDictionary<Int32, MyData>
+    {
+    
+    }
+    
+    public MyDictionary myDic = new MyDictionary()
+    {
+        {12, new MyData() {key = "FirstValue", name = "dfsdfds"}}
+    };
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
