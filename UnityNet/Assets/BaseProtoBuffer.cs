09f31632e4d5ca03627b8205328f4bb927cba66d
hexinping <hexinping456@126.com>
2021-10-25 13:23:48 +0800

    update

---
+++ UnityNet/Assets/BaseProtoBuffer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseProtoBuffer : MonoBehaviour
+{
+    // Start is called before the first frame update‘
+
+    [Serializable]
+    public class  Person
+    {
+        public int age = 10;
+        public string name = "dd";
+    }
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
