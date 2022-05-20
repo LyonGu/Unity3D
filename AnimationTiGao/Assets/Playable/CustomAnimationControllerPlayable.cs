20ab372e6efe76d8f9b7e649d7512aae0e32837e
hexinping <hexinping456@126.com>
2022-05-20 14:59:15 +0800

    [AnimationTiGao] 添加自定义PlayableBehaviour

---
+++ AnimationTiGao/Assets/Playable/CustomAnimationControllerPlayable.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.Playables;
+public class CustomAnimationControllerPlayable : PlayableBehaviour
+{
+
+    private int m_CurrentClipIndex = -1;
+    private float m_TimeToNextClip;
+    private Playable mixer;
+    public void Initialize(AnimationClip[] clipsToPlay, Playable owner, PlayableGraph graph)
+    {
+        //设置owner 输入个数
+        owner.SetInputCount(1);
+        mixer = AnimationMixerPlayable.Create(graph, clipsToPlay.Length);
+        // mixer往owner上连
+        graph.Connect(mixer, 0, owner, 0);
+        owner.SetInputWeight(0, 1);
+        for (int clipIndex = 0; clipIndex < mixer.GetInputCount(); ++clipIndex)
+        {
+            AnimationClipPlayable clipPlayable = AnimationClipPlayable.Create(graph, clipsToPlay[clipIndex]);
+            // clipPlayable往mixer上连
+            graph.Connect(clipPlayable, 0, mixer, clipIndex);
+            mixer.SetInputWeight(clipIndex, 1.0f);
+        }
+    }
+
+    //每一帧刷新之前会调用 在每一帧对Playable中的元素进行访问和设置
+    override public void PrepareFrame(Playable owner, FrameData info)
+    {
+        if (mixer.GetInputCount() == 0)
+            return;
+        // Advance to next clip if necessary
+        m_TimeToNextClip -= (float)info.deltaTime;
+        if (m_TimeToNextClip <= 0.0f)
+        {
+            m_CurrentClipIndex++;
+            if (m_CurrentClipIndex >= mixer.GetInputCount())
+                m_CurrentClipIndex = 0;
+            var currentClip = (AnimationClipPlayable)mixer.GetInput(m_CurrentClipIndex);
+            // Reset the time so that the next clip starts at the correct position
+            currentClip.SetTime(0);
+            m_TimeToNextClip = currentClip.GetAnimationClip().length;
+        }
+        // Adjust the weight of the inputs
+        for (int clipIndex = 0; clipIndex < mixer.GetInputCount(); ++clipIndex)
+        {
+            if (clipIndex == m_CurrentClipIndex)
+                mixer.SetInputWeight(clipIndex, 1.0f);
+            else
+                mixer.SetInputWeight(clipIndex, 0.0f);
+        }
+    }
+
+}
---
+++ AnimationTiGao/Assets/Playable/CustomAnimationControllor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.Playables;
+
+public class CustomAnimationControllor : MonoBehaviour
+{
+
+    public AnimationClip[] clipsToPlay;
+    PlayableGraph m_Graph;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Graph = PlayableGraph.Create("CustomAnimationControllor1");
+
+        var custPlayable = ScriptPlayable<CustomAnimationControllerPlayable>.Create(m_Graph);
+
+        var playQueue = custPlayable.GetBehaviour();
+
+        playQueue.Initialize(clipsToPlay, custPlayable, m_Graph);
+
+        var playableOutput = AnimationPlayableOutput.Create(m_Graph, "CustomAnimationOutput", GetComponent<Animator>());
+
+        //设置SourcePlayable为自定义的custPlayable
+        playableOutput.SetSourcePlayable(custPlayable);
+        playableOutput.SetSourceInputPort(0);
+
+        m_Graph.Play();
+    }
+
+    // Update is called once per frame
+    private void OnDestroy()
+    {
+        if (m_Graph.IsValid())
+            m_Graph.Destroy();
+    }
+}
