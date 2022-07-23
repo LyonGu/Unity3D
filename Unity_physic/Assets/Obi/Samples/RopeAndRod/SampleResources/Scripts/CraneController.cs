370ec87b3cc725d61f0b480130da0d3fe8308402
hexinping <hexinping456@126.com>
2022-07-23 12:51:06 +0800

    【unity物理】  obi rope  在手机上试下

--- Unity_physic/Assets/Obi/Samples/RopeAndRod/SampleResources/Scripts/CraneController.cs
+++ Unity_physic/Assets/Obi/Samples/RopeAndRod/SampleResources/Scripts/CraneController.cs
@@ -13,24 +13,62 @@ public class CraneController : MonoBehaviour {
 		cursor = GetComponentInChildren<ObiRopeCursor>();
 		rope = cursor.GetComponent<ObiRope>();
 	}
-	
+
+
+	private Vector3 _prePos;
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.W)){
-			if (rope.restLength > 6.5f)
-				cursor.ChangeLength(rope.restLength - 1f * Time.deltaTime);
+		
+		//原始插件代码
+		// if (Input.GetKey(KeyCode.W)){
+		// 	if (rope.restLength > 6.5f)
+		// 		cursor.ChangeLength(rope.restLength - 1f * Time.deltaTime);
+		// }
+		//
+		// if (Input.GetKey(KeyCode.S)){
+		// 	cursor.ChangeLength(rope.restLength + 1f * Time.deltaTime);
+		// }
+		//
+		// if (Input.GetKey(KeyCode.A)){
+		// 	transform.Rotate(0,Time.deltaTime*15f,0);
+		// }
+		//
+		// if (Input.GetKey(KeyCode.D)){
+		// 	transform.Rotate(0,-Time.deltaTime*15f,0);
+		// }
+		
+		//自己修改测试
+		if (Input.GetMouseButtonDown(0))
+		{
+			_prePos = Input.mousePosition;
+		}
+		else if (Input.GetMouseButton(0))
+		{
+			var detal = Input.mousePosition - _prePos;
+			if (detal.x > 0)
+			{
+				transform.Rotate(0,-Time.deltaTime*15f,0);
+			}
+			else if(detal.x < 0)
+			{
+				transform.Rotate(0,Time.deltaTime*15f,0);
+			}
+
+			if (detal.y > 0)
+			{
+				if (rope.restLength > 6.5f)
+					cursor.ChangeLength(rope.restLength - 1f * Time.deltaTime);
+			}
+			else if (detal.y < 0)
+			{
+				cursor.ChangeLength(rope.restLength + 1f * Time.deltaTime);
+			}
+
+			_prePos = Input.mousePosition;
 		}
 
-		if (Input.GetKey(KeyCode.S)){
-			cursor.ChangeLength(rope.restLength + 1f * Time.deltaTime);
-		}
 
-		if (Input.GetKey(KeyCode.A)){
-			transform.Rotate(0,Time.deltaTime*15f,0);
-		}
 
-		if (Input.GetKey(KeyCode.D)){
-			transform.Rotate(0,-Time.deltaTime*15f,0);
-		}
+		// Debug.Log($"X Z========={x}  {z}");
 	}
 }
