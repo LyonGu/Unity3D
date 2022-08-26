7bb968d0591b1fa9ce126d5326dd94e5a27401ca
hexinping <hexinping456@126.com>
2022-08-26 13:47:33 +0800

    千人战斗场景优化

---
+++ 一些整理的代码/千人战斗场景优化/classOpt/classOpt/Assets/Scripts/GameApp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameApp : MonoBehaviour {
+
+    public GameObject archerPrefab = null;
+
+	// Use this for initialization
+	void Start () { // 20x40 --->800个小兵;
+        float z = 0;
+        for (int i = 0; i < 20; i++) {
+            float x = 0;
+            for (int j = 0; j < 40; j++) {
+                GameObject archer = GameObject.Instantiate(this.archerPrefab);
+                archer.transform.SetParent(this.transform, false);
+                archer.transform.position = new Vector3(x, 0, z);
+                x ++;
+            }
+            z += 1;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
