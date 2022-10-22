a54b67e41352b39ea886c513c362010c4301db53
hexinping <hexinping456@126.com>
2022-10-22 22:52:40 +0800

    add

---
+++ XNode/Assets/NewNodeGraph.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+[CreateAssetMenu(fileName = "NewNodeGraph", menuName = "xNode Examples/NewNodeGraph")]
+public class NewNodeGraph : NodeGraph { 
+	
+}
