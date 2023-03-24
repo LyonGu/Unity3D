787029c1aae003bc5b733edb8a081a5a91184c36
hexinping <hexinping456@126.com>
2023-03-24 18:48:41 +0800

    update

---
+++ AnimationTiGao/Assets/TestLoadFBX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLoadFBX : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // GameObject 
+        GameObject fbxObjRes = Resources.Load("unitychan1") as GameObject;
+        GameObject fbxObj = Instantiate(fbxObjRes);
+        fbxObj.transform.SetParent(transform, true);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
