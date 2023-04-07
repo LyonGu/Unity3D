528d529faf2e2561ebe11a7c87f96fb94cb58ce6
hexinping <hexinping456@126.com>
2023-04-07 17:58:11 +0800

    [DoTween] DoJump Test

---
+++ DOTweenPro/Assets/Scripts/DoTweenTest.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class DoTweenTest : MonoBehaviour
+{
+
+    public Transform StartTrans;
+
+    public Transform EndTrans;
+
+    public Transform TargetTrans;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TargetTrans.position = StartTrans.position;
+            TargetTrans.DOJump(EndTrans.position, 5, 1, 2.0f, false);
+        }
+    }
+}
