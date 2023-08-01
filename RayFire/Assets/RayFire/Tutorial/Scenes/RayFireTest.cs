9b82decc88460b86a54811d61acd2573daa7e626
hexinping <hexinping456@126.com>
2023-08-01 11:27:33 +0800

    Ray Fire 笔记：事件添加

--- RayFire/Assets/RayFire/Tutorial/Scenes/RayFireTest.cs
+++ RayFire/Assets/RayFire/Tutorial/Scenes/RayFireTest.cs
@@ -1,18 +1,78 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using RayFire;
 using UnityEngine;
 
 public class RayFireTest : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private RayfireRigid _rayfirwRigid;
+
+    private Dictionary<int, RayfireRigid> _rayfireRigidDic = new Dictionary<int, RayfireRigid>(32);
+    private void Awake()
     {
-        
+        _rayfirwRigid = GetComponent<RayfireRigid>();
+        AddListeners();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
+        int a = 10;
+        if (_rayfirwRigid.fragments != null)
+        {
+            foreach (var rayfireRigid in _rayfirwRigid.fragments)
+            {
+                int id = rayfireRigid.GetInstanceID();
+                if (!_rayfireRigidDic.ContainsKey(id))
+                {
+                    _rayfireRigidDic.Add(id, rayfireRigid);
+                }
+            }
+        }
+    }
+
+    private void AddListeners()
+    {
+        // if (_rayfirwRigid != null)
+        // {
+        //     _rayfirwRigid.activationEvent.LocalEvent += MyMethod;
+        // }
         
+        RFActivationEvent.GlobalEvent += MyMethod;
+    }
+    
+    private void RemoveListeners()
+    {
+        // if (_rayfirwRigid != null)
+        // {
+        //     _rayfirwRigid.activationEvent.LocalEvent -=MyMethod;
+        // }
+        RFActivationEvent.GlobalEvent -= MyMethod;
+    }
+
+    private void OnDestroy()
+    {
+        RemoveListeners();
+    }
+
+    private void Update()
+    {
+        int a = 10;
+    }
+
+    void MyMethod(RayfireRigid rigid)
+    {
+        // if(rigid!=null && rigid.fragments!=null)
+        //     Debug.Log($"{Time.frameCount} : {rigid.gameObject.name}================{rigid.fragments.Count}");
+
+        int id = rigid.GetInstanceID();
+        if (_rayfireRigidDic.ContainsKey(id))
+        {
+            Debug.Log($"{Time.frameCount} : {rigid.gameObject.name} 被激活================");
+            rigid.gameObject.AddComponent<RayFireCustomeCom>();
+        }
+
+       
     }
 }
